0b6738e4662d12eec154c016bf27ea3a3b69c08e
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-07-19 14:31:33 +0200

    Adapt to new project structure

--- liqd_product/config/wsgi.py
+++ liqd_product/config/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "liqd_produkt.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "liqd_product.config.settings")
 
 application = get_wsgi_application()
