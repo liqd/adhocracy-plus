96bbb386ec1ec58e04601846a2367a00eabc3ef5
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-07-26 15:36:05 +0200

    Add partner tests

---
+++ tests/partners/test_middleware.py
@@ -0,0 +1,41 @@
+import pytest
+from django.http import Http404
+
+from liqd_product.apps.partners import get_partner
+from liqd_product.apps.partners import set_partner
+from liqd_product.apps.partners.middleware import PartnerMiddleware
+
+
+@pytest.mark.django_db
+def test_middleware_set_partner(rf, partner):
+    slug = partner.slug
+    request = rf.get('/%s/' % slug)
+    middleware = PartnerMiddleware()
+
+    middleware.process_view(request, None, None, {
+        'partner_slug': slug
+    })
+
+    assert get_partner() == partner
+
+
+@pytest.mark.django_db
+def test_middleware_partner_not_found(rf):
+    request = rf.get('/unknown/')
+    middleware = PartnerMiddleware()
+
+    with pytest.raises(Http404):
+        middleware.process_view(request, None, None, {
+            'partner_slug': 'unknown'
+        })
+
+
+@pytest.mark.django_db
+def test_middleware_clear_partner(rf, partner):
+    request = rf.get('/login/')
+    middleware = PartnerMiddleware()
+
+    set_partner(partner)
+    middleware.process_view(request, None, None, {})
+
+    assert get_partner() is None
---
+++ tests/partners/test_urlresolver.py
@@ -0,0 +1,65 @@
+import pytest
+from django.conf.urls import include
+from django.conf.urls import url
+from django.views.defaults import page_not_found
+
+from liqd_product.apps.partners import set_partner
+from liqd_product.apps.partners.urlresolvers import partner_patterns
+from liqd_product.apps.partners.urlresolvers import reverse
+
+base_urlconf = [
+    url('^static/', page_not_found, name='static-url'),
+    url('^dynamic/(?P<pk>\d+)/', page_not_found, name='dynamic-url'),
+]
+
+
+@pytest.mark.django_db
+def test_partner_patterns(partner_factory):
+    partner = partner_factory(slug='automatic_partner')
+    set_partner(partner)
+    urlconf = (partner_patterns(
+        *base_urlconf
+    ),)
+
+    url_ = reverse('static-url', urlconf)
+    assert url_ == '/automatic_partner/static/'
+
+    url_ = reverse('static-url', urlconf, kwargs=dict(
+        partner_slug='manual_partner'))
+    assert url_ == '/manual_partner/static/'
+
+    url_ = reverse('dynamic-url', urlconf, kwargs=dict(pk=1))
+    assert url_ == '/automatic_partner/dynamic/1/'
+
+    url_ = reverse('dynamic-url', urlconf, kwargs=dict(
+        partner_slug='manual_partner', pk=1))
+    assert url_ == '/manual_partner/dynamic/1/'
+
+    url_ = reverse('dynamic-url', urlconf, args=(1,))
+    assert url_ == '/automatic_partner/dynamic/1/'
+
+
+@pytest.mark.django_db
+def test_partner_patterns_instance_ns(partner_factory):
+    partner = partner_factory(slug='automatic_partner')
+    set_partner(partner)
+    urlconf = (partner_patterns(
+        url(r'^ns/', include(base_urlconf, namespace='instance-ns'))
+    ),)
+
+    url_ = reverse('instance-ns:static-url', urlconf)
+    assert url_ == '/automatic_partner/ns/static/'
+
+    url_ = reverse('instance-ns:static-url', urlconf, kwargs=dict(
+        partner_slug='manual_partner'))
+    assert url_ == '/manual_partner/ns/static/'
+
+    url_ = reverse('instance-ns:dynamic-url', urlconf, kwargs=dict(pk=1))
+    assert url_ == '/automatic_partner/ns/dynamic/1/'
+
+    url_ = reverse('instance-ns:dynamic-url', urlconf, kwargs=dict(
+        partner_slug='manual_partner', pk=1))
+    assert url_ == '/manual_partner/ns/dynamic/1/'
+
+    url_ = reverse('instance-ns:dynamic-url', urlconf, args=(1,))
+    assert url_ == '/automatic_partner/ns/dynamic/1/'
