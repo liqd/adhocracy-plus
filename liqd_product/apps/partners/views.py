0d234b3b8d21a622266785f2b179d9d431c1a8a0
Tobias Bengfort <tobias.bengfort@liqd.de>
2017-07-31 17:15:50 +0200

    add activity block

--- liqd_product/apps/partners/views.py
+++ liqd_product/apps/partners/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import DetailView
 
+from adhocracy4.actions.models import Action
 from liqd_product.apps.partners.models import Partner
 
 
@@ -7,3 +8,9 @@ class PartnerView(DetailView):
     template_name = 'partner_landing_page.html'
     model = Partner
     slug_url_kwarg = 'partner_slug'
+
+    def get_context_data(self, **kwargs):
+        context = super(PartnerView, self).get_context_data(**kwargs)
+        # FIXME: limit to current partner
+        context['action_list'] = Action.objects.all()[:10]
+        return context
