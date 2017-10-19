94b6e357777d4fe0437ad6b6541764301d5ca346
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-10-19 11:29:11 +0200

    Add A4_CATEGORIZABLE setting

--- liqd_product/config/settings/base.py
+++ liqd_product/config/settings/base.py
@@ -307,6 +307,13 @@ A4_AUTO_FOLLOWABLES = (
     ('meinberlin_mapideas', 'mapidea'),
 )
 
+
+A4_CATEGORIZABLE = (
+    ('meinberlin_ideas', 'idea'),
+    ('meinberlin_mapideas', 'mapidea'),
+)
+
+
 A4_MAP_BASEURL = 'https://{s}.tile.openstreetmap.org/'
 A4_MAP_ATTRIBUTION = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 A4_MAP_BOUNDING_BOX = ([[52.3517, 13.8229], [52.6839, 12.9543]])
