ea03d42bb9be8c5ba9c940a568e395eef65f438a
Robert Mader <robert.mader@liqd.de>
2018-03-28 14:28:05 +0200

    add whitenoise support and enable it

--- liqd_product/config/settings/production.py
+++ liqd_product/config/settings/production.py
@@ -2,6 +2,8 @@ from .base import *
 
 DEBUG = False
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 try:
     from .local import *
 except ImportError:
---
+++ liqd_product/config/whitenoise.py
@@ -0,0 +1,13 @@
+"""WSGI WhiteNoise config for Beteiligung.in """
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "liqd_product.config.settings")
+
+application = get_wsgi_application()
+
+application = DjangoWhiteNoise(application)
