8a45c86ca536e9d280319f16adde32e4fddcc408
Irae Hueck Costa <i.hueck-costa@liqd.net>
2018-04-18 11:43:07 +0200

    Change default map bounding box to germany, closes #218

--- liqd_product/config/settings/base.py
+++ liqd_product/config/settings/base.py
@@ -404,7 +404,7 @@ A4_CATEGORY_ICONS = (
 
 A4_MAP_BASEURL = 'https://{s}.tile.openstreetmap.org/'
 A4_MAP_ATTRIBUTION = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-A4_MAP_BOUNDING_BOX = ([[52.3517, 13.8229], [52.6839, 12.9543]])
+A4_MAP_BOUNDING_BOX = ([[54.983, 15.016], [47.302, 5.988]])
 
 A4_DASHBOARD = {
     'PROJECT_DASHBOARD_CLASS': 'meinberlin.apps.dashboard.ProjectDashboard',
