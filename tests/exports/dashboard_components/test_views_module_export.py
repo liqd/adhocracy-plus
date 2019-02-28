2478d47d6c3762e932615d348d9ecdd2d98cbd31
fuzzylogic2000 <katharina.albers@liqd.de>
2019-02-28 18:18:54 +0100

    add tests for exports, moderatorremark, and some rules

---
+++ tests/exports/dashboard_components/test_views_module_export.py
@@ -0,0 +1,30 @@
+import pytest
+
+from adhocracy4.dashboard import components
+from liqd_product.apps.ideas.phases import CollectFeedbackPhase
+from tests.helpers import setup_phase
+
+component = components.modules.get('idea_export')
+
+
+@pytest.mark.django_db
+def test_edit_view(client, phase_factory):
+    phase, module, project, item = setup_phase(
+        phase_factory, None, CollectFeedbackPhase)
+    initiator = module.project.organisation.initiators.first()
+    url = component.get_base_url(module)
+    client.login(username=initiator.email, password='password')
+    response = client.get(url)
+    assert response.status_code == 200
+    assert 'export' in response.context
+    assert 'comment_export' in response.context
+    export_url = response.context['export']
+    comment_export_url = response.context['comment_export']
+    response = client.get(export_url)
+    assert response.status_code == 200
+    assert response.get('Content-Type') == \
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    response = client.get(comment_export_url)
+    assert response.status_code == 200
+    assert response.get('Content-Type') == \
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
---
+++ tests/exports/test_item_exports.py
@@ -0,0 +1,40 @@
+import pytest
+
+from liqd_product.apps.exports import mixins
+
+
+@pytest.mark.django_db
+def test_moderator_feedback_mixin(proposal):
+    mixin = mixins.ItemExportWithModeratorFeedback()
+
+    virtual = mixin.get_virtual_fields({})
+    assert 'moderator_feedback' in virtual
+    assert 'moderator_statement' in virtual
+
+    assert mixin.get_moderator_feedback_data(proposal)\
+        == proposal.get_moderator_feedback_display()
+
+    assert mixin.get_moderator_statement_data(proposal)\
+        == proposal.moderator_statement.statement
+
+
+@pytest.mark.django_db
+def test_user_generated_content_mixin(idea):
+    mixin = mixins.UserGeneratedContentExportMixin()
+
+    virtual = mixin.get_virtual_fields({})
+    assert 'creator' in virtual
+    assert 'created' in virtual
+
+    assert idea.creator.username == mixin.get_creator_data(idea)
+    assert idea.created.isoformat() == mixin.get_created_data(idea)
+
+
+@pytest.mark.django_db
+def test_reference_number_mixin(idea):
+    mixin = mixins.ItemExportWithReferenceNumberMixin()
+
+    virtual = mixin.get_virtual_fields({})
+    assert 'reference_number' in virtual
+
+    assert idea.reference_number == mixin.get_reference_number_data(idea)
