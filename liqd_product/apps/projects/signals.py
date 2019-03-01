790fa478c106cbfe7538acaa4647355bdd8ccdb5
Magdalena Noffke <magdalena.noffke@liqd.de>
2019-03-01 15:03:17 +0100

    use post_delete signal

--- liqd_product/apps/projects/signals.py
+++ liqd_product/apps/projects/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import pre_delete
+from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
 from adhocracy4.projects.models import Project
@@ -6,6 +6,6 @@ from adhocracy4.projects.models import Project
 from .emails import DeleteProjectEmail
 
 
-@receiver(pre_delete, sender=Project)
+@receiver(post_delete, sender=Project)
 def sendDeleteProjectNotification(sender, instance, **kwargs):
     DeleteProjectEmail.send_no_object(instance)
