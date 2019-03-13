0b3515d8244b804be8ff37ad70527d452e643caa
Robert Mader <r.mader@liqd.net>
2019-03-13 16:58:24 +0100

    add on_delete=models.CASCADE to some migrations

--- liqd_product/apps/budgeting/migrations/0005_inherit_from_moderateable.py
+++ liqd_product/apps/budgeting/migrations/0005_inherit_from_moderateable.py
@@ -23,6 +23,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='proposal',
             name='moderator_statement',
-            field=models.OneToOneField(null=True, related_name='+', to='liqd_product_moderatorfeedback.ModeratorStatement'),
+            field=models.OneToOneField(null=True, related_name='+', to='liqd_product_moderatorfeedback.ModeratorStatement', on_delete=models.CASCADE),
         ),
     ]
