8678bb15f8960cfab6d7575f039733659a7d8d0f
Robert Mader <r.mader@liqd.net>
2019-05-21 15:17:20 +0200

    apps/django_overwrites: Update label

--- apps/django_overwrites/apps.py
+++ apps/django_overwrites/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class Config(AppConfig):
     name = 'apps.django_overwrites'
-    label = 'liqd_product_django_overwrites'
+    label = 'a4_candy_django_overwrites'
