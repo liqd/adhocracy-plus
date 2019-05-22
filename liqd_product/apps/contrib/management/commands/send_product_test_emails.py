383760f7a707aae9dae12120d2e7719c90c3f665
Magdalena Noffke <magdalena.noffke@liqd.de>
2019-05-22 17:15:30 +0200

    apps remove all imports to removed email.py and use Email from a4

--- liqd_product/apps/contrib/management/commands/send_product_test_emails.py
+++ liqd_product/apps/contrib/management/commands/send_product_test_emails.py
@@ -6,11 +6,11 @@ from django.core.management.base import BaseCommand
 from adhocracy4.actions.models import Action
 from adhocracy4.actions.verbs import Verbs
 from adhocracy4.comments.models import Comment
+from adhocracy4.emails import Email
 from adhocracy4.emails.mixins import SyncEmailMixin
 from adhocracy4.projects.models import Project
 from adhocracy4.reports import emails as reports_emails
 from adhocracy4.reports.models import Report
-from liqd_product.apps.contrib.emails import Email
 from liqd_product.apps.ideas.models import Idea
 from liqd_product.apps.notifications import emails as notification_emails
 from liqd_product.apps.projects import models as project_models
