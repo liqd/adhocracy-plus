6fb1931536da08991e721511fdac41a511fad6ae
Robert Mader <r.mader@liqd.net>
2019-06-27 13:40:13 +0200

    apps/users: Set language on login

--- apps/users/__init__.py
+++ apps/users/__init__.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.signals import user_logged_in
 from django.utils.translation import ugettext_lazy as _
 
 default_app_config = 'apps.users.apps.Config'
@@ -7,3 +8,11 @@ USERNAME_INVALID_MESSAGE = _('Enter a valid username. This value may contain '
                              'only letters, digits, spaces and @/./+/-/_ '
                              'characters. It must start with a digit or a '
                              'letter.')
+
+
+def set_language(sender, user, request, **kwargs):
+    from .utils import set_session_language
+    set_session_language(user, None, request)
+
+
+user_logged_in.connect(set_language)
