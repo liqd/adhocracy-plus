dc746ace4c9b32b1ff7e29b35969f0027fd7d3e5
Magdalena Noffke <magdalena.noffke@liqd.de>
2019-07-15 17:16:57 +0200

    users/adapters: add missing parameter to is_safe_url

--- apps/users/adapters.py
+++ apps/users/adapters.py
@@ -34,7 +34,8 @@ class AccountAdapter(DefaultAccountAdapter):
 
     def get_email_confirmation_url(self, request, emailconfirmation):
         url = super().get_email_confirmation_url(request, emailconfirmation)
-        if 'next' in request.POST and is_safe_url(request.POST['next']):
+        if 'next' in request.POST and is_safe_url(request.POST['next'],
+                                                  allowed_hosts=None):
             return '{}?next={}'.format(url, quote(request.POST['next']))
         else:
             return url
@@ -47,7 +48,8 @@ class AccountAdapter(DefaultAccountAdapter):
         )
 
     def get_email_confirmation_redirect_url(self, request):
-        if 'next' in request.GET and is_safe_url(request.GET['next']):
+        if 'next' in request.GET and is_safe_url(request.GET['next'],
+                                                 allowed_hosts=None):
             return request.GET['next']
         else:
             return super().get_email_confirmation_redirect_url(request)
