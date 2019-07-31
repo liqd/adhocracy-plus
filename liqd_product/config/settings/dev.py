342aed2c3147c387642003171d67052f9573a7b7
Phillippa <p.morland@liqd.net>
2019-07-31 14:17:41 +0200

    tidy dev and bg colour

--- liqd_product/config/settings/dev.py
+++ liqd_product/config/settings/dev.py
@@ -9,15 +9,15 @@ for template_engine in TEMPLATES:
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'k3l^@x53$l5@y(fo6ivgplj&q^^rmtl^enzse5ozxuepx0$s()'
 
-# try:
-#     import debug_toolbar
-# except ImportError:
-#     pass
-# else:
-#     INSTALLED_APPS += ('debug_toolbar',)
-#     MIDDLEWARE += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
-#
-#     INTERNAL_IPS = ('127.0.0.1', 'localhost')
+try:
+    import debug_toolbar
+except ImportError:
+    pass
+else:
+    INSTALLED_APPS += ('debug_toolbar',)
+    MIDDLEWARE += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+
+    INTERNAL_IPS = ('127.0.0.1', 'localhost')
 
 try:
     from .local import *
