6f532bea784dcbbf1acc02119f93d8fb4dd2fccf
Magdalena Noffke <magdalena.noffke@liqd.de>
2019-08-08 15:08:38 +0200

    organisations: Remove unneeded functionality

--- apps/organisations/templatetags/organisation_tags.py
+++ apps/organisations/templatetags/organisation_tags.py
@@ -3,29 +3,7 @@ from django.urls import resolve
 
 from apps.organisations.models import Organisation
 
-from .. import organisation_context
-
 register = template.Library()
-
-
-class WithOrganisationNode(template.Node):
-    def __init__(self, nodelist, organisation):
-        self.nodelist = nodelist
-        self.organisation = template.Variable(organisation)
-
-    def render(self, context):
-        actual_organisation = self.organisation.resolve(context)
-        with organisation_context(actual_organisation):
-            output = self.nodelist.render(context)
-        return output
-
-
-@register.tag
-def withorganisation(parser, token):
-    tag_name, organisation = token.split_contents()
-    nodelist = parser.parse(('endwithorganisation',))
-    parser.delete_first_token()
-    return WithOrganisationNode(nodelist, organisation)
 
 
 @register.simple_tag(takes_context=True)
