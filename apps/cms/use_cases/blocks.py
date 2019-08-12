50e8cd6f73c2e62134eb9d61429cf2470d72a996
Magdalena Noffke <magdalena.noffke@liqd.de>
2019-08-12 16:20:53 +0200

    cms: Fix linting errors

--- apps/cms/use_cases/blocks.py
+++ apps/cms/use_cases/blocks.py
@@ -3,9 +3,11 @@ from wagtail.core import blocks
 
 class UseCaseBlock(blocks.StructBlock):
     title = blocks.CharBlock()
-    use_cases = blocks.ListBlock(blocks.PageChooserBlock(target_model='a4_candy_cms_use_cases.UseCasePage'))
+    use_cases = blocks.ListBlock(blocks.PageChooserBlock(
+        target_model='a4_candy_cms_use_cases.UseCasePage'))
     demo_platform = blocks.URLBlock()
-    use_case_page = blocks.PageChooserBlock(target_model='a4_candy_cms_use_cases.UseCaseIndexPage')
+    use_case_page = blocks.PageChooserBlock(
+        target_model='a4_candy_cms_use_cases.UseCaseIndexPage')
 
     class Meta:
         template = 'a4_candy_cms_use_cases/blocks/use_cases_block.html'
