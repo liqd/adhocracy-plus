df9d97486b47dc1f2ffec5a03f6c22e3e5d480f2
fuzzylogic2000 <katharina.albers@liqd.de>
2019-08-14 09:59:48 +0200

    pagination: set to intended values

--- apps/ideas/views.py
+++ apps/ideas/views.py
@@ -55,7 +55,7 @@ class IdeaFilterSet(a4_filters.DefaultsFilterSet):
 
 class AbstractIdeaListView(ProjectMixin,
                            filter_views.FilteredListView):
-    paginate_by = 2
+    paginate_by = 15
 
 
 class IdeaListView(AbstractIdeaListView, DisplayProjectOrModuleMixin):
