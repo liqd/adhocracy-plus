162d00613ed633f526c50357bb0485367661299e
Robert Mader <r.mader@liqd.net>
2019-09-26 12:57:55 +0200

    apps/organisations: Except unresolvable paths
    If a path does not exists, e.g. on not existing static files, the template
    tag would throw a `Resolver404` exception, crashing the base template, which
    again gets used by the 404 template, which leads to a 500 error instead of
    404.

--- apps/organisations/templatetags/organisation_tags.py
+++ apps/organisations/templatetags/organisation_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.urls import Resolver404
 from django.urls import resolve
 
 from apps.organisations.models import Organisation
@@ -9,10 +10,10 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def get_current_organisation(context):
     request = context.request
-    resolver = resolve(request.path_info)
-    if resolver.kwargs and 'organisation_slug' in resolver.kwargs:
-        try:
+    try:
+        resolver = resolve(request.path_info)
+        if resolver.kwargs and 'organisation_slug' in resolver.kwargs:
             return Organisation.objects.get(
                 slug=resolver.kwargs['organisation_slug'])
-        except Organisation.DoesNotExist:
-            pass
+    except (Resolver404, Organisation.DoesNotExist):
+        pass
