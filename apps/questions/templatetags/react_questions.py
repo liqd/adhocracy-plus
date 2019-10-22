6c1cb72619ff528c9d1c1a6ddc72973432779736
Magdalena Noffke <magdalena.noffke@liqd.de>
2019-10-22 15:44:14 +0200

    apps/questions: remove template_tag for rendering statisticBox component

--- apps/questions/templatetags/react_questions.py
+++ apps/questions/templatetags/react_questions.py
@@ -18,45 +18,32 @@ def react_questions(context, obj):
     categories = [category.name for category in obj.category_set.all()]
     questions_api_url = reverse('questions-list', kwargs={'module_pk': obj.pk})
 
-    permission = 'a4_candy_likes.add_like_model'
+    like_permission = 'a4_candy_likes.add_like_model'
     has_liking_permission = user.has_perm(
-        permission, obj)
+        like_permission, obj)
     would_have_liking_permission = NormalUser().would_have_perm(
-        permission, obj
+        like_permission, obj
     )
 
+    ask_permissions = 'a4_candy_questions.propose_question'
+    has_ask_questions_permissions = user.has_perm(ask_permissions, obj)
+    would_have_ask_questions_permission = NormalUser().would_have_perm(
+        ask_permissions, obj)
+
     attributes = {
+        'information': obj.description,
         'questions_api_url': questions_api_url,
         'isModerator': is_moderator,
         'categories': categories,
         'hasLikingPermission': (has_liking_permission
-                                or would_have_liking_permission)
+                                or would_have_liking_permission),
+        'hasAskQuestionsPermission': (has_ask_questions_permissions
+                                      or would_have_ask_questions_permission),
+        'askQuestionUrl': reverse('question-create', kwargs={'slug': obj.slug})
     }
 
     return format_html(
         '<div data-speakup-widget="questions" '
-        'data-attributes="{attributes}"></div>',
-        attributes=json.dumps(attributes)
-    )
-
-
-@register.simple_tag(takes_context=True)
-def react_questions_statistics(context, obj):
-    request = context['request']
-
-    categories = [category.name for category in obj.category_set.all()]
-    questions_api_url = reverse('questions-list', kwargs={'module_pk': obj.pk})
-    user = request.user
-    is_moderator = user.has_perm('a4_candy_questions.moderate_questions', obj)
-
-    attributes = {
-        'questions_api_url': questions_api_url,
-        'categories': categories,
-        'isModerator': is_moderator,
-    }
-
-    return format_html(
-        '<div data-speakup-widget="statistics" '
         'data-attributes="{attributes}"></div>',
         attributes=json.dumps(attributes)
     )
