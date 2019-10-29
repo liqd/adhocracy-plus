62178fe9f60816a141f0abcc7accfedb42d9aa29
Robert Mader <r.mader@liqd.net>
2019-10-29 17:56:29 +0100

    ci: Use postgresql instead of sqlite

--- adhocracy-plus/config/settings/test.py
+++ adhocracy-plus/config/settings/test.py
@@ -4,3 +4,14 @@ A4_ORGANISATION_FACTORY = 'tests.factories.OrganisationFactory'
 A4_USER_FACTORY = 'tests.factories.UserFactory'
 
 ACCOUNT_EMAIL_VERIFICATION = 'optional'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'postgres',
+        'NAME': 'django',
+        'TEST': {
+            'NAME': 'django_test'
+        },
+    }
+}
