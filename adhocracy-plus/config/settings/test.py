8f40a5585549a6d8f48d10cf4e7bbedfd6c6f445
Robert Mader <r.mader@liqd.net>
2019-10-30 10:00:22 +0100

    ci: Move travis db config out of general test config

--- adhocracy-plus/config/settings/test.py
+++ adhocracy-plus/config/settings/test.py
@@ -4,14 +4,3 @@ A4_ORGANISATION_FACTORY = 'tests.factories.OrganisationFactory'
 A4_USER_FACTORY = 'tests.factories.UserFactory'
 
 ACCOUNT_EMAIL_VERIFICATION = 'optional'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'USER': 'postgres',
-        'NAME': 'django',
-        'TEST': {
-            'NAME': 'django_test'
-        },
-    }
-}
