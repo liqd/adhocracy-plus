liqd/adhocracy-plus

commit 47d1db2b62fb5161da70674e75dcb36db9917297

assets/timestamps.js: Use minified version of timeago

apps/actions/assets/timestamps.js
@@ -1,5 +1,4 @@
-const timeago = require('timeago.js')
-require('timeago.js/src/timeago.locales.js')
+const timeago = require('timeago.js/dist/timeago.min.js')
 const $ = require('jquery')
 
 $(function () {

commit cecf7770bf349b5f5a83bfb23c62f3d16757ad63

apps/actions: Fix timeago call
This broke in 47d1db2b62fb516 or the commit before.

apps/actions/assets/timestamps.js
@@ -7,10 +7,9 @@ $(function () {
   $times.each((i, e) => {
     const sevenDays = 60 * 60 * 24 * 7 * 1000
     const datetime = new Date($(e).attr('datetime'))
-    const timeagoInstance = timeago()
 
     if ((new Date() - datetime) < sevenDays) {
-      e.textContent = timeagoInstance.format(datetime, document.documentElement.lang)
+      e.textContent = timeago.format(datetime, document.documentElement.lang)
     }
   })
 })