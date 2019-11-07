dd266f5ee88951207a006675ec39577478b2e3a2
Robert Mader <r.mader@liqd.net>
2019-11-07 11:40:36 +0100

    users/forms: Overwrite allauth form instead of using custom forms
    This makes our registration flow saver and easier.
    Also, add a "username" field for social logins.

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,69 +1,40 @@
+from allauth.account.forms import SignupForm
 from allauth.socialaccount.adapter import get_adapter
-from allauth.utils import email_address_exists
+from allauth.socialaccount.forms import SignupForm as SocialSignupForm
 from django import forms
-from django.contrib.auth import forms as auth_forms
-from django.contrib.auth import get_user_model
 from django.utils.translation import ugettext_lazy as _
 
-User = get_user_model()
 
-
-class TermsSignupForm(auth_forms.UserCreationForm):
-    terms_of_use = forms.BooleanField(label=_('Terms of use'), error_messages={
-        'required': _('Please accept the terms of use.')
-    })
-
-    def signup(self, request, user):
-        user.get_newsletters = self.cleaned_data["get_newsletters"]
-        user.signup(
-            self.cleaned_data['username'],
-            self.cleaned_data['email'],
-        )
-
-    class Meta:
-        model = User
-        fields = ('email', 'username', 'password1', 'password2',
-                  'terms_of_use', 'get_newsletters')
-
-# Tried to add form as described in allauth documentation:
-# https://django-allauth.readthedocs.io/en/latest/forms.html#socialaccount-forms
-# ran into the following error:
-# https://stackoverflow.com/questions/57254251/custom-form-with-socialaccount-in-django-allauth
-# added this solution, maybe not the best
-
-
-class SignupForm(forms.Form):
+class TermsSignupForm(SignupForm):
     terms_of_use = forms.BooleanField(label=_('Terms of use'), error_messages={
         'required': _('Please accept the terms of use.')
     })
     get_newsletters = forms.BooleanField(
         label=_('Send me newsletters'), required=False)
+
+    def save(self, request):
+        user = super(TermsSignupForm, self).save(request)
+        user.get_newsletters = self.cleaned_data['get_newsletters']
+        user.save()
+        return user
+
+
+class SocialTermsSignupForm(SocialSignupForm):
     email = forms.EmailField(widget=forms.HiddenInput())
-    username = forms.CharField(widget=forms.HiddenInput())
+    terms_of_use = forms.BooleanField(label=_('Terms of use'), error_messages={
+        'required': _('Please accept the terms of use.')
+    })
+    get_newsletters = forms.BooleanField(
+        label=_('Send me newsletters'), required=False)
 
     def __init__(self, *args, **kwargs):
-        self.sociallogin = kwargs.pop('sociallogin')
         initial = get_adapter().get_signup_form_initial_data(
-            self.sociallogin)
-        kwargs.update({
-            'initial': initial})
+            kwargs["sociallogin"])
+        kwargs.update({'initial': initial})
         super().__init__(*args, **kwargs)
 
     def save(self, request):
-        adapter = get_adapter(request)
-        user = adapter.save_user(request, self.sociallogin, form=self)
+        user = super(SocialTermsSignupForm, self).save(request)
         user.get_newsletters = self.cleaned_data['get_newsletters']
         user.save()
-        user.signup(
-            user.username,
-            user.email
-        )
         return user
-
-    def clean(self):
-        email = self.cleaned_data['email']
-        if email_address_exists(email):
-            raise forms.ValidationError(
-                get_adapter().error_messages['email_taken']
-                % self.sociallogin.account.get_provider().name)
-        return super().clean()
