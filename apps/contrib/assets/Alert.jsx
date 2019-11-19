17999191970412734548864c3f4d03a08df46ecb
Phillippa <p.morland@liqd.net>
2019-11-19 11:36:20 +0100

    classname: l-wrapper replaced with container

--- apps/contrib/assets/Alert.jsx
+++ apps/contrib/assets/Alert.jsx
@@ -5,7 +5,7 @@ const Alert = ({ type, message, onClick }) => {
   if (type) {
     return (
       <div className={`alert alert--${type}`} role="alert">
-        <div className="l-wrapper">
+        <div className="container">
           {message}
           <button className="alert__close" title={django.gettext('Close')} onClick={onClick}>
             <i className="fa fa-times" aria-label={django.gettext('Close')} />
