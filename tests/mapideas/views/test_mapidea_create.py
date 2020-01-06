3f1b512324a838f0000f77f69696757e60fded58
fuzzylogic2000 <katharina.albers@liqd.de>
2020-01-06 15:57:54 +0100

    tests/mapideas: move all view tests to separate files

--- tests/mapideas/views/test_mapidea_create.py
+++ tests/mapideas/views/test_mapidea_create.py
@@ -6,6 +6,35 @@ from apps.mapideas import models
 from apps.mapideas import phases
 from tests.helpers import assert_template_response
 from tests.helpers import freeze_phase
+from tests.helpers import setup_phase
+
+
+@pytest.mark.django_db
+def test_create_view(client, phase_factory, map_idea_factory, user,
+                     category_factory, area_settings_factory, organisation):
+    phase, module, project, item = setup_phase(
+        phase_factory, map_idea_factory, phases.CollectPhase)
+    area_settings_factory(module=module)
+    category = category_factory(module=module)
+    url = reverse('a4_candy_mapideas:mapidea-create',
+                  kwargs={'organisation_slug': organisation.slug,
+                          'module_slug': module.slug})
+    with freeze_phase(phase):
+        client.login(username=user.email, password='password')
+
+        response = client.get(url)
+        assert_template_response(
+            response, 'a4_candy_mapideas/mapidea_create_form.html')
+
+        data = {
+            'name': 'Idea',
+            'description': 'description',
+            'category': category.pk,
+            'point': (0, 0),
+            'point_label': 'somewhere'
+        }
+        response = client.post(url, data)
+        assert redirect_target(response) == 'mapidea-detail'
 
 
 @pytest.mark.django_db
