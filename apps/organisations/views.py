442f4ed1de90da205a14f56d3cbfe5141df4e5df
Magdalena Noffke <magdalena.noffke@liqd.de>
2020-01-23 15:01:04 +0100

    apps/organisations: Retrieve lists in view and add to context

--- apps/organisations/views.py
+++ apps/organisations/views.py
@@ -3,10 +3,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.views import generic
 from django.views.generic import DetailView
 
-from adhocracy4.actions.models import Action
-from adhocracy4.projects.models import Project
 from adhocracy4.rules import mixins as rules_mixins
-from apps.projects import query
 
 from . import forms
 from .models import Organisation
@@ -19,17 +16,12 @@ class OrganisationView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        active, future, past = \
+            self.object.get_projects_list(self.request.user)
 
-        project_list = Project.objects\
-            .filter(organisation=self.object,
-                    is_archived=False,
-                    is_draft=False)
-        project_list = query.filter_viewable(
-            project_list, self.request.user
-        )
-        context['project_list'] = project_list
-
-
+        context['active_projects'] = active
+        context['future_projects'] = future
+        context['past_projects'] = past
         return context
 
 
