d16eff7a947a1f20e4757419e254a63b40a34b22
fuzzylogic2000 <katharina.albers@liqd.de>
2020-01-24 14:27:57 +0100

    tests/debate: add tests for the creator count properties

--- tests/debate/test_subject_models.py
+++ tests/debate/test_subject_models.py
@@ -46,3 +46,103 @@ def test_delete_subject(subject_factory, comment_factory, rating_factory):
     comment_count = comments_models.Comment.objects.all().count()
     assert count == 0
     assert comment_count == 0
+
+
+@pytest.mark.django_db
+def test_comment_creator_count(subject_factory, comment_factory,
+                               user_factory):
+    subject1 = subject_factory()
+    subject2 = subject_factory()
+    subject3 = subject_factory()
+
+    user1 = user_factory()
+    user2 = user_factory()
+    user3 = user_factory()
+    user4 = user_factory()
+    user5 = user_factory()
+
+    comment_factory(content_object=subject1, creator=user1)
+    comment_factory(content_object=subject1, creator=user1)
+    comment_factory(content_object=subject1, creator=user2)
+    comment_factory(content_object=subject1, creator=user2)
+    comment_factory(content_object=subject1, creator=user3)
+    comment_factory(content_object=subject1, creator=user4)
+    comment_factory(content_object=subject1, creator=user5)
+
+    comment_factory(content_object=subject2, creator=user1)
+    comment_factory(content_object=subject2, creator=user1)
+    comment_factory(content_object=subject2, creator=user2)
+    comment_factory(content_object=subject2, creator=user2)
+    comment_factory(content_object=subject2, creator=user3)
+
+    assert subject1.comment_creator_count == 5
+    assert subject2.comment_creator_count == 3
+    assert subject3.comment_creator_count == 0
+
+
+@pytest.mark.django_db
+def test_comment_creator_count_minus_three(subject_factory, comment_factory,
+                                           user_factory):
+    subject1 = subject_factory()
+    subject2 = subject_factory()
+    subject3 = subject_factory()
+
+    user1 = user_factory()
+    user2 = user_factory()
+    user3 = user_factory()
+    user4 = user_factory()
+    user5 = user_factory()
+
+    comment_factory(content_object=subject1, creator=user1)
+    comment_factory(content_object=subject1, creator=user1)
+    comment_factory(content_object=subject1, creator=user2)
+    comment_factory(content_object=subject1, creator=user2)
+    comment_factory(content_object=subject1, creator=user3)
+    comment_factory(content_object=subject1, creator=user4)
+    comment_factory(content_object=subject1, creator=user5)
+
+    comment_factory(content_object=subject2, creator=user1)
+    comment_factory(content_object=subject2, creator=user1)
+    comment_factory(content_object=subject2, creator=user2)
+    comment_factory(content_object=subject2, creator=user2)
+    comment_factory(content_object=subject2, creator=user3)
+
+    assert subject1.comment_creator_count_minus_three == 2
+    assert subject2.comment_creator_count_minus_three is None
+    assert subject3.comment_creator_count_minus_three is None
+
+
+@pytest.mark.django_db
+def test_last_three_creators(subject_factory, comment_factory,
+                             user_factory):
+    subject1 = subject_factory()
+    subject2 = subject_factory()
+    subject3 = subject_factory()
+
+    user1 = user_factory()
+    user2 = user_factory()
+    user3 = user_factory()
+    user4 = user_factory()
+    user5 = user_factory()
+
+    comment_factory(content_object=subject1, creator=user1)
+    comment_factory(content_object=subject1, creator=user1)
+    comment_factory(content_object=subject1, creator=user2)
+    comment_factory(content_object=subject1, creator=user2)
+    comment_factory(content_object=subject1, creator=user3)
+    comment_factory(content_object=subject1, creator=user4)
+    comment_factory(content_object=subject1, creator=user5)
+
+    comment_factory(content_object=subject2, creator=user1)
+    comment_factory(content_object=subject2, creator=user1)
+    comment_factory(content_object=subject2, creator=user2)
+    comment_factory(content_object=subject2, creator=user2)
+    comment_factory(content_object=subject2, creator=user3)
+
+    assert user3 in subject1.last_three_creators
+    assert user4 in subject1.last_three_creators
+    assert user5 in subject1.last_three_creators
+    assert user1 in subject2.last_three_creators
+    assert user2 in subject2.last_three_creators
+    assert user3 in subject2.last_three_creators
+    assert len(subject3.last_three_creators) == 0
