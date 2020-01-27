c0ebfa160785731f95d7782444a0a262a2ec37b8
Philli <phillippamorland@gmail.com>
2020-01-27 14:43:26 +0100

    Revert "assets: Big app.js / webpack cleanup from mB"
    This reverts commit 7fb729b3535ab13f6954804b9612a91a6cee9aab.

--- apps/embed/urls.py
+++ apps/embed/urls.py
@@ -5,6 +5,4 @@ from . import views
 urlpatterns = [
     url(r'^projects/(?P<slug>[-\w_]+)/$', views.EmbedProjectView.as_view(),
         name='embed-project'),
-    url('login_success', views.EmbedLoginClose.as_view(),
-        name='embed-login-success'),
 ]
