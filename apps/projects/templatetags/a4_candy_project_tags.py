999daf78672d2c6629becd463bb36622c5545b76
fuzzylogic2000 <katharina.albers@liqd.de>
2020-02-05 13:51:27 +0100

    apps/debate: count comments of debating module for tile

--- apps/projects/templatetags/a4_candy_project_tags.py
+++ apps/projects/templatetags/a4_candy_project_tags.py
@@ -33,5 +33,6 @@ def get_num_entries(module):
         + Comment.objects.filter(chapter__module=module).count() \
         + Comment.objects.filter(poll__module=module).count() \
         + Comment.objects.filter(topic__module=module).count() \
+        + Comment.objects.filter(subject__module=module).count() \
         + Vote.objects.filter(choice__question__poll__module=module).count()
     return item_count
