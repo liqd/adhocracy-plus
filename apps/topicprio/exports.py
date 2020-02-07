a20fa72f2ace18bca989b000cdd6d7d9d43a17b6
fuzzylogic2000 <katharina.albers@liqd.de>
2020-02-07 12:02:36 +0100

    apps/exports: add reference number to comment export

--- apps/topicprio/exports.py
+++ apps/topicprio/exports.py
@@ -45,6 +45,7 @@ class TopicCommentExportView(PermissionRequiredMixin,
                              a4_export_mixins.ExportModelFieldsMixin,
                              export_mixins.UserGeneratedContentExportMixin,
                              a4_export_mixins.ItemExportWithRatesMixin,
+                             export_mixins.ReferenceExportWithRepliesToMixin,
                              export_mixins.CommentExportWithRepliesToMixin,
                              a4_export_views.BaseItemExportView):
 
