e9490bfbf3bf6d786bf270ab57fe0377d7a93aa0
fuzzylogic2000 <katharina.albers@liqd.de>
2020-02-12 16:45:04 +0100

    apps/organisations: add member model and make members addable in django-admin

--- apps/organisations/admin.py
+++ apps/organisations/admin.py
@@ -3,7 +3,29 @@ from django.contrib import admin
 from . import models
 
 
+class MemberInline(admin.TabularInline):
+    model = models.Member
+    fields = [
+        'member',
+    ]
+    raw_id_fields = ('member',)
+    extra = 1
+
+
 @admin.register(models.Organisation)
 class OrganisationAdmin(admin.ModelAdmin):
     search_fields = ('name',)
+    list_display = (
+        'id', 'name'
+    )
     raw_id_fields = ('initiators',)
+    inlines = [
+        MemberInline,
+    ]
+
+
+@admin.register(models.Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', '__str__', 'organisation', 'member'
+    )
