d73e41a19f8c29c93b49dd6bd43480fd7f8e5208
fuzzylogic2000 <katharina.albers@liqd.de>
2020-02-25 15:53:48 +0100

    apps/captcha: enable captcha

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -8,6 +8,8 @@ from django.utils.translation import get_language
 from django.utils.translation import ugettext_lazy as _
 from zeep import Client
 
+from apps.captcha.fields import CaptcheckCaptchaField
+from apps.captcha.mixins import CaptcheckCaptchaFormMixin
 from apps.organisations.models import Member
 from apps.organisations.models import Organisation
 
@@ -21,7 +23,7 @@ class DefaultLoginForm(LoginForm):
         del self.fields['password'].widget.attrs['placeholder']
 
 
-class DefaultSignupForm(SignupForm):
+class DefaultSignupForm(CaptcheckCaptchaFormMixin, SignupForm):
     terms_of_use = forms.BooleanField(
         label=_('Terms of use')
     )
@@ -31,6 +33,7 @@ class DefaultSignupForm(SignupForm):
                     'additional information via email.'),
         required=False
     )
+    captcha = CaptcheckCaptchaField()
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
