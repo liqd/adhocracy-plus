6ce0a50c26e439a897e6aa3edea9914df2e36d7b
fuzzylogic2000 <katharina.albers@liqd.de>
2020-02-27 13:57:15 +0100

    tests/organisations: test member model properties and in views and rules

--- tests/organisations/test_organisation_detail_view.py
+++ tests/organisations/test_organisation_detail_view.py
@@ -33,8 +33,10 @@ def test_hide_private_projects(client, user, project_factory,
         assert response.status_code == 200
 
         project_list = response.context['active_projects']
+        project_headline = response.context['project_headline']
         assert public in project_list
         assert private not in project_list
+        assert project_headline == 'Participate now!'
 
 
 @pytest.mark.django_db
@@ -61,16 +63,18 @@ def test_show_private_projects_participant(client, user, project_factory,
         end_date=parse('2013-01-01 19:00:00 UTC')
     )
 
-    with freeze_time(parse('2013-01-01 18:00:00 UTC')):
+    with freeze_time(parse('2013-01-01 19:05:00 UTC')):
         client.login(username=user, password='password')
         url = reverse('organisation',
                       kwargs={'organisation_slug': organisation.slug})
         response = client.get(url)
         assert response.status_code == 200
 
-        project_list = response.context['active_projects']
+        project_list = response.context['past_projects']
+        project_headline = response.context['project_headline']
         assert public in project_list
         assert private in project_list
+        assert project_headline == 'Ended participation'
 
 
 @pytest.mark.django_db
@@ -97,13 +101,54 @@ def test_show_private_projects_initiators(client, user, project_factory,
         end_date=parse('2013-01-01 19:00:00 UTC')
     )
 
-    with freeze_time(parse('2013-01-01 18:00:00 UTC')):
+    with freeze_time(parse('2013-01-01 17:00:00 UTC')):
         client.login(username=user, password='password')
         url = reverse('organisation',
                       kwargs={'organisation_slug': organisation.slug})
         response = client.get(url)
         assert response.status_code == 200
 
-        project_list = response.context['active_projects']
+        project_list = response.context['future_projects']
+        project_headline = response.context['project_headline']
         assert public in project_list
         assert private in project_list
+        assert project_headline == 'Upcoming participation'
+
+
+@pytest.mark.django_db
+def test_show_private_projects_members(client, member, project_factory,
+                                       module_factory, phase_factory):
+    organisation = member.organisation
+    public = project_factory(organisation=organisation)
+    private = project_factory(is_public=False, organisation=organisation)
+
+    module1 = module_factory(project=public, weight=1)
+    module2 = module_factory(project=private, weight=2)
+
+    phase_factory(
+        module=module1,
+        start_date=parse('2013-01-01 17:10:00 UTC'),
+        end_date=parse('2013-01-01 18:05:00 UTC'),
+    )
+
+    phase_factory(
+        module=module2,
+        start_date=parse('2013-01-01 17:05:00 UTC'),
+        end_date=parse('2013-01-01 19:00:00 UTC')
+    )
+
+    with freeze_time(parse('2013-01-01 17:07:00 UTC')):
+        client.login(username=member.member, password='password')
+        url = reverse('organisation',
+                      kwargs={'organisation_slug': organisation.slug})
+        response = client.get(url)
+        assert response.status_code == 200
+
+        active_projects = response.context['active_projects']
+        future_projects = response.context['future_projects']
+        project_headline = response.context['project_headline']
+        assert public not in active_projects
+        assert private in active_projects
+        assert public in future_projects
+        assert private not in future_projects
+        assert project_headline == 'Participate now!'
