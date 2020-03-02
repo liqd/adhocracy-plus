55a4712f2b43e9ee8215bfa5c68f3444e66963c4
fuzzylogic2000 <katharina.albers@liqd.de>
2020-03-02 17:31:44 +0100

    tests/rules: tests for member role

--- tests/polls/rules/test_rules_change_poll.py
+++ tests/polls/rules/test_rules_change_poll.py
@@ -16,10 +16,11 @@ def test_perm_exists():
 
 
 @pytest.mark.django_db
-def test_pre_phase(phase_factory, poll_factory, user):
+def test_pre_phase(phase_factory, poll_factory, user, member_factory):
     phase, _, project, item = setup_phase(phase_factory, poll_factory,
                                           phases.VotingPhase)
     anonymous, moderator, initiator = setup_users(project)
+    member = member_factory(organisation=project.organisation)
 
     creator = item.creator
 
@@ -27,56 +28,65 @@ def test_pre_phase(phase_factory, poll_factory, user):
     with freeze_pre_phase(phase):
         assert not rules.has_perm(perm_name, anonymous, item)
         assert not rules.has_perm(perm_name, user, item)
+        assert not rules.has_perm(perm_name, member.member, item)
         assert not rules.has_perm(perm_name, creator, item)
         assert rules.has_perm(perm_name, moderator, item)
         assert rules.has_perm(perm_name, initiator, item)
 
 
 @pytest.mark.django_db
-def test_phase_active(phase_factory, poll_factory, user):
+def test_phase_active(phase_factory, poll_factory, user, member_factory):
     phase, _, project, item = setup_phase(phase_factory, poll_factory,
                                           phases.VotingPhase)
     anonymous, moderator, initiator = setup_users(project)
+    member = member_factory(organisation=project.organisation)
     creator = item.creator
 
     assert project.is_public
     with freeze_phase(phase):
         assert not rules.has_perm(perm_name, anonymous, item)
         assert not rules.has_perm(perm_name, user, item)
+        assert not rules.has_perm(perm_name, member.member, item)
         assert not rules.has_perm(perm_name, creator, item)
         assert rules.has_perm(perm_name, moderator, item)
         assert rules.has_perm(perm_name, initiator, item)
 
 
 @pytest.mark.django_db
-def test_phase_active_project_draft(phase_factory, poll_factory, user):
+def test_phase_active_project_draft(phase_factory, poll_factory, user,
+                                    member_factory):
     phase, _, project, item = setup_phase(phase_factory, poll_factory,
                                           phases.VotingPhase,
                                           module__project__is_draft=True)
     anonymous, moderator, initiator = setup_users(project)
+    member = member_factory(organisation=project.organisation)
     creator = item.creator
 
     assert project.is_draft
     with freeze_phase(phase):
         assert not rules.has_perm(perm_name, anonymous, item)
         assert not rules.has_perm(perm_name, user, item)
+        assert not rules.has_perm(perm_name, member.member, item)
         assert not rules.has_perm(perm_name, creator, item)
         assert rules.has_perm(perm_name, moderator, item)
         assert rules.has_perm(perm_name, initiator, item)
 
 
 @pytest.mark.django_db
-def test_post_phase_project_archived(phase_factory, poll_factory, user):
+def test_post_phase_project_archived(phase_factory, poll_factory, user,
+                                     member_factory):
     phase, _, project, item = setup_phase(phase_factory, poll_factory,
                                           phases.VotingPhase,
                                           module__project__is_archived=True)
     anonymous, moderator, initiator = setup_users(project)
+    member = member_factory(organisation=project.organisation)
     creator = item.creator
 
     assert project.is_archived
     with freeze_post_phase(phase):
         assert not rules.has_perm(perm_name, anonymous, item)
         assert not rules.has_perm(perm_name, user, item)
+        assert not rules.has_perm(perm_name, member.member, item)
         assert not rules.has_perm(perm_name, creator, item)
         assert rules.has_perm(perm_name, moderator, item)
         assert rules.has_perm(perm_name, initiator, item)
