231f4377603d6bc61e558cf06501b6a2861df3d1
fuzzylogic2000 <katharina.albers@liqd.de>
2020-03-02 17:31:44 +0100

    tests/rules: tests for member role

--- tests/budgeting/rules/test_rules_view.py
+++ tests/budgeting/rules/test_rules_view.py
@@ -16,77 +16,89 @@ def test_perm_exists():
 
 
 @pytest.mark.django_db
-def test_pre_phase(phase_factory, proposal_factory, user):
+def test_pre_phase(phase_factory, proposal_factory, user, member_factory):
     phase, _, project, item = setup_phase(phase_factory, proposal_factory,
                                           phases.RequestPhase)
     anonymous, moderator, initiator = setup_users(project)
+    member = member_factory(organisation=project.organisation)
 
     assert project.is_public
     with freeze_pre_phase(phase):
         assert rules.has_perm(perm_name, anonymous, item)
         assert rules.has_perm(perm_name, user, item)
+        assert rules.has_perm(perm_name, member.member, item)
         assert rules.has_perm(perm_name, moderator, item)
         assert rules.has_perm(perm_name, initiator, item)
 
 
 @pytest.mark.django_db
-def test_phase_active(phase_factory, proposal_factory, user):
+def test_phase_active(phase_factory, proposal_factory, user, member_factory):
     phase, _, project, item = setup_phase(phase_factory, proposal_factory,
                                           phases.RequestPhase)
     anonymous, moderator, initiator = setup_users(project)
+    member = member_factory(organisation=project.organisation)
 
     assert project.is_public
     with freeze_phase(phase):
         assert rules.has_perm(perm_name, anonymous, item)
         assert rules.has_perm(perm_name, user, item)
+        assert rules.has_perm(perm_name, member.member, item)
         assert rules.has_perm(perm_name, moderator, item)
         assert rules.has_perm(perm_name, initiator, item)
 
 
 @pytest.mark.django_db
 def test_phase_active_project_private(phase_factory, proposal_factory,
-                                      user, user2):
+                                      user, user2, member_factory):
     phase, _, project, item = setup_phase(phase_factory, proposal_factory,
                                           phases.RequestPhase,
                                           module__project__is_public=False)
     anonymous, moderator, initiator = setup_users(project)
     participant = user2
     project.participants.add(participant)
+    member = member_factory(organisation=project.organisation)
 
     assert not project.is_public
     with freeze_phase(phase):
         assert not rules.has_perm(perm_name, anonymous, item)
         assert not rules.has_perm(perm_name, user, item)
+        assert rules.has_perm(perm_name, member.member, item)
         assert rules.has_perm(perm_name, participant, item)
         assert rules.has_perm(perm_name, moderator, item)
         assert rules.has_perm(perm_name, initiator, item)
 
 
 @pytest.mark.django_db
-def test_phase_active_project_draft(phase_factory, proposal_factory, user):
+def test_phase_active_project_draft(phase_factory, proposal_factory, user,
+                                    member_factory):
     phase, _, project, item = setup_phase(phase_factory, proposal_factory,
                                           phases.RequestPhase,
                                           module__project__is_draft=True)
     anonymous, moderator, initiator = setup_users(project)
+    member = member_factory(organisation=project.organisation)
 
     assert project.is_draft
     with freeze_phase(phase):
         assert not rules.has_perm(perm_name, anonymous, item)
         assert not rules.has_perm(perm_name, user, item)
+        assert not rules.has_perm(perm_name, member.member, item)
         assert rules.has_perm(perm_name, moderator, item)
         assert rules.has_perm(perm_name, initiator, item)
 
 
 @pytest.mark.django_db
-def test_post_phase_project_archived(phase_factory, proposal_factory, user):
+def test_post_phase_project_archived(phase_factory, proposal_factory, user,
+                                     member_factory):
     phase, _, project, item = setup_phase(phase_factory, proposal_factory,
                                           phases.RequestPhase,
                                           module__project__is_archived=True)
     anonymous, moderator, initiator = setup_users(project)
+    member = member_factory(organisation=project.organisation)
 
     assert project.is_archived
     with freeze_post_phase(phase):
         assert rules.has_perm(perm_name, anonymous, item)
         assert rules.has_perm(perm_name, user, item)
+        assert rules.has_perm(perm_name, member.member, item)
         assert rules.has_perm(perm_name, moderator, item)
         assert rules.has_perm(perm_name, initiator, item)
--- tests/offlineevents/rules/test_rules_view.py
+++ tests/offlineevents/rules/test_rules_view.py
@@ -11,37 +11,62 @@ def test_perm_exists():
 
 
 @pytest.mark.django_db
-def test_rule(offline_event, user):
+def test_rule(offline_event, user, member_factory):
     project = offline_event.project
     anonymous, moderator, initiator = setup_users(project)
+    member = member_factory(organisation=project.organisation)
 
     assert rules.has_perm(perm_name, anonymous, offline_event)
     assert rules.has_perm(perm_name, user, offline_event)
+    assert rules.has_perm(perm_name, member.member, offline_event)
     assert rules.has_perm(perm_name, moderator, offline_event)
     assert rules.has_perm(perm_name, initiator, offline_event)
 
 
 @pytest.mark.django_db
-def test_rule_project_draft(offline_event_factory, user):
+def test_rule_project_private(offline_event_factory, user, user2,
+                              member_factory):
+    offline_event = offline_event_factory(project__is_public=False)
+    project = offline_event.project
+    anonymous, moderator, initiator = setup_users(project)
+    member = member_factory(organisation=project.organisation)
+    participant = user2
+    project.participants.add(participant)
+
+    assert not project.is_public
+    assert not rules.has_perm(perm_name, anonymous, offline_event)
+    assert not rules.has_perm(perm_name, user, offline_event)
+    assert rules.has_perm(perm_name, member.member, offline_event)
+    assert rules.has_perm(perm_name, participant, offline_event)
+    assert rules.has_perm(perm_name, moderator, offline_event)
+    assert rules.has_perm(perm_name, initiator, offline_event)
+
+
+@pytest.mark.django_db
+def test_rule_project_draft(offline_event_factory, user, member_factory):
     offline_event = offline_event_factory(project__is_draft=True)
     project = offline_event.project
     anonymous, moderator, initiator = setup_users(project)
+    member = member_factory(organisation=project.organisation)
 
     assert project.is_draft
     assert not rules.has_perm(perm_name, anonymous, offline_event)
     assert not rules.has_perm(perm_name, user, offline_event)
+    assert not rules.has_perm(perm_name, member.member, offline_event)
     assert rules.has_perm(perm_name, moderator, offline_event)
     assert rules.has_perm(perm_name, initiator, offline_event)
 
 
 @pytest.mark.django_db
-def test_rule_project_archived(offline_event_factory, user):
+def test_rule_project_archived(offline_event_factory, user, member_factory):
     offline_event = offline_event_factory(project__is_archived=True)
     project = offline_event.project
     anonymous, moderator, initiator = setup_users(project)
+    member = member_factory(organisation=project.organisation)
 
     assert project.is_archived
     assert rules.has_perm(perm_name, anonymous, offline_event)
     assert rules.has_perm(perm_name, user, offline_event)
+    assert rules.has_perm(perm_name, member.member, offline_event)
     assert rules.has_perm(perm_name, moderator, offline_event)
     assert rules.has_perm(perm_name, initiator, offline_event)
