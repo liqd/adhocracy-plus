e8faa962838f771644cd388e5ac8fe5fb15fdb3c
Rineee <k.schmid@liqd.net>
2020-03-03 15:40:38 +0100

    apps/debate: use textarea for subject description in dashboard form

--- apps/debate/forms.py
+++ apps/debate/forms.py
@@ -8,6 +8,7 @@ class SubjectForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         self.module = kwargs.pop('module')
         super().__init__(*args, **kwargs)
+        self.fields['description'].widget = forms.Textarea({'rows': 4})
 
     class Meta:
         model = models.Subject
