9b604fbeeb5fae4fead451678f02ed616edb47db
Phillippa <p.morland@liqd.net>
2020-03-05 15:39:26 +0100

    settings/base.py: add blank attribute to link in ck so links open in new tab

--- adhocracy-plus/config/settings/base.py
+++ adhocracy-plus/config/settings/base.py
@@ -312,13 +312,13 @@ BLEACH_LIST = {
     'default' : {
         'tags': ['p','strong','em','u','ol','li','ul','a'],
         'attributes': {
-            'a': ['href', 'rel'],
+            'a': ['href', 'rel', 'target'],
         },
     },
     'image-editor': {
         'tags': ['p','strong','em','u','ol','li','ul','a','img'],
         'attributes': {
-            'a': ['href', 'rel'],
+            'a': ['href', 'rel', 'target'],
             'img': ['src', 'alt', 'style']
         },
         'styles': [
@@ -337,7 +337,7 @@ BLEACH_LIST = {
         'tags': ['p', 'strong', 'em', 'u', 'ol', 'li', 'ul', 'a', 'img',
                  'div'],
         'attributes': {
-            'a': ['href', 'rel'],
+            'a': ['href', 'rel', 'target'],
             'img': ['src', 'alt', 'style'],
             'div': ['class']
         },
