a1fbcddffbf08bdfd4a93b895ddaa07bca714904
Rineee <k.schmid@liqd.net>
2020-04-06 15:02:41 +0200

    apps/organisations: split organisation edit form into two to have separate form for legal information

--- apps/organisations/forms.py
+++ apps/organisations/forms.py
@@ -7,12 +7,22 @@ class OrganisationForm(forms.ModelForm):
 
     class Meta:
         model = Organisation
-        fields = ['title', 'slogan', 'description', 'information', 'imprint',
+        fields = ['title', 'slogan', 'description', 'information',
                   'logo', 'image', 'image_copyright', 'url',
-                  'twitter_handle', 'facebook_handle', 'instagram_handle',
-                  'terms_of_use', 'data_protection', 'netiquette']
+                  'twitter_handle', 'facebook_handle', 'instagram_handle']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['description'].widget = forms.Textarea({'rows': 4})
         self.fields['slogan'].widget = forms.Textarea({'rows': 2})
+
+
+class OrganisationLegalInformationForm(forms.ModelForm):
+
+    class Meta:
+        model = Organisation
+        fields = ['imprint', 'terms_of_use', 'data_protection',
+                  'netiquette']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
