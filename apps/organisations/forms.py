1ee0c3783ef566bea7758f1da8ac7bb78661ff3e
fuzzylogic2000 <katharina.albers@liqd.de>
2020-04-20 11:19:58 +0200

    apps/organisations: add cms-links from settings to helptexts of legal fields

--- apps/organisations/forms.py
+++ apps/organisations/forms.py
@@ -1,6 +1,15 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+from apps.cms.settings import helpers
 
 from .models import Organisation
+
+IMPRINT_HELP = _('Here you can find an example of an {}imprint{}.')
+TERMS_OF_USE_HELP = _('Here you can find an example of {}terms of use{}.')
+DATA_PROTECTION_HELP = _('Here you can find an example of a '
+                         '{}data protection policy{}.')
+NETIQUETTE_HELP = _('Here you can find an example of a {}netiquette{}.')
 
 
 class OrganisationForm(forms.ModelForm):
@@ -24,3 +33,20 @@ class OrganisationLegalInformationForm(forms.ModelForm):
         model = Organisation
         fields = ['imprint', 'terms_of_use', 'data_protection',
                   'netiquette']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['imprint'].help_text = helpers.add_link_to_helptext(
+            self.fields['imprint'].help_text, "imprint",
+            IMPRINT_HELP)
+        self.fields['terms_of_use'].help_text = helpers.add_link_to_helptext(
+            self.fields['terms_of_use'].help_text, "terms_of_use",
+            TERMS_OF_USE_HELP)
+        self.fields['data_protection'].help_text \
+            = helpers.add_link_to_helptext(
+                self.fields['data_protection'].help_text,
+                "data_protection_policy",
+                DATA_PROTECTION_HELP)
+        self.fields['netiquette'].help_text = helpers.add_link_to_helptext(
+            self.fields['netiquette'].help_text, "netiquette",
+            NETIQUETTE_HELP)
