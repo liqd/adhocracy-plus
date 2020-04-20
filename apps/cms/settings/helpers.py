1d78f445e0d7ed2419c72a4d99b14aae4ac1a705
fuzzylogic2000 <katharina.albers@liqd.de>
2020-04-20 11:19:58 +0200

    apps/organisations: add cms-links from settings to helptexts of legal fields

---
+++ apps/cms/settings/helpers.py
@@ -0,0 +1,25 @@
+from django.utils.html import mark_safe
+from django.utils.translation import ugettext_lazy as _
+from wagtail.core.models import Site
+
+from apps.cms.settings.models import ImportantPages
+
+LINK_TEXT = _('Please look {}here{} for more information.')
+
+
+def add_link_to_helptext(help_text, important_page_name, link_text=None):
+    site = Site.objects.filter(
+        is_default_site=True
+    ).first()
+    important_pages = ImportantPages.for_site(site)
+
+    if getattr(important_pages, important_page_name) and \
+       getattr(important_pages, important_page_name).live:
+        url = getattr(important_pages, important_page_name).url
+        if not link_text:
+            link_text = LINK_TEXT
+        link_text = link_text \
+            .format('<a href="' + url + '" target="_blank">', '</a>')
+        return '{} {}'.format(help_text, mark_safe(link_text))
+
+    return help_text
