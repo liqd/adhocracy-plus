c8a08a3dbd615a95c7604ace1dbc59a16578e70d
fuzzylogic2000 <katharina.albers@liqd.de>
2020-04-28 16:52:18 +0200

    tests/eports: add test for comment categories export mixin

--- tests/exports/test_comment_exports.py
+++ tests/exports/test_comment_exports.py
@@ -35,3 +35,28 @@ def test_reply_to_reference_mixin(idea, comment_factory):
     assert mixin.get_replies_to_reference_data(comment) \
         == idea.reference_number
     assert mixin.get_replies_to_reference_data(reply_comment) == ''
+
+
+@pytest.mark.django_db
+def test_comment_export_with_categories_mixin(idea, comment_factory):
+    mixin = mixins.CommentExportWithCategoriesMixin()
+
+    virtual = mixin.get_virtual_fields({})
+    assert 'categories' in virtual
+
+    comment = comment_factory(
+        content_object=idea,
+        comment_categories='sug'
+    )
+    comment2 = comment_factory(
+        content_object=idea,
+        comment_categories='sug,not'
+    )
+    reply_comment = comment_factory(content_object=comment)
+
+    assert Comment.objects.count() == 3
+
+    assert mixin.get_categories_data(comment) == 'suggestion'
+    assert 'suggestion' in mixin.get_categories_data(comment2)
+    assert 'note' in mixin.get_categories_data(comment2)
+    assert mixin.get_categories_data(reply_comment) == ''
