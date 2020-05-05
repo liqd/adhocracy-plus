6ad741eff313fe93ee8b625436e0fb81c251f1f4
fuzzylogic2000 <katharina.albers@liqd.de>
2020-05-05 19:57:14 +0200

    apps/dashboard: add empty project without module

--- apps/dashboard/forms.py
+++ apps/dashboard/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
+from adhocracy4.dashboard.forms import ProjectCreateForm
+from adhocracy4.projects import models as project_models
 from apps.organisations.models import Organisation
 
 
@@ -11,4 +13,19 @@ class OrganisationForm(forms.ModelForm):
         fields = ['name', 'logo']
         labels = {
             'name': _('Organisation name')
+        }
+
+
+class DashboardProjectCreateForm(ProjectCreateForm):
+
+    class Meta:
+        model = project_models.Project
+        fields = ['name', 'description', 'is_public']
+        widgets = {
+            'is_public': forms.RadioSelect(
+                choices=[
+                    (True, _('All users can participate (public).')),
+                    (False, _('Only invited users can participate (private).'))
+                ]
+            )
         }
