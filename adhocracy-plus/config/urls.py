17f31043ed7b7938614d4743e657426191f8042d
Robert Mader <r.mader@liqd.net>
2020-08-20 18:32:15 +0200

    config: Enable saml2 and add some configs

--- adhocracy-plus/config/urls.py
+++ adhocracy-plus/config/urls.py
@@ -120,6 +120,8 @@ urlpatterns = [
     path('robots.txt',
          TemplateView.as_view(template_name='robots.txt',
                               content_type="text/plain"), name="robots_file"),
+
+    re_path(r'^saml2/', include('djangosaml2.urls')),
 ]
 
 
