f697841897887ccd5dceaaa9ddfaf132b511c39b
fuzzylogic2000 <katharina.albers@liqd.de>
2020-10-08 17:13:36 +0200

    tests/organisations: test update translated fields

--- tests/organisations/test_organisation_update_view.py
+++ tests/organisations/test_organisation_update_view.py
@@ -1,5 +1,6 @@
 import pytest
 from django.urls import reverse
+from parler.utils.context import switch_language
 
 from adhocracy4.test.helpers import redirect_target
 from apps.organisations import models
@@ -17,16 +18,74 @@ def test_initiator_can_update(client, organisation):
         'title': 'Organisation platform title',
         'language': 'de',
         'en': 'en',
-        'en__description': 'some very short description of the organisation'
+        'en__description': 'some very short description of the organisation',
+        'en__slogan': 'some slogan in English',
+        'en__information': 'This is very important info!',
+        'de': 'de',
+        'de__description': 'Eine sehr kurze Beschreibung der Organisation',
+        'de__slogan': 'Slogan auf Deutsch',
+        'de__information': 'Ein paar wichtige Informationen',
     }
     response = client.post(url, data)
     assert redirect_target(response) == 'organisation-settings'
     assert response.status_code == 302
-    updated_organisation = models.Organisation.objects.get(id=organisation.id)
-    assert updated_organisation.title == 'Organisation platform title'
-    assert updated_organisation.description == \
+    organisation.refresh_from_db()
+    organisation.get_translation('en').refresh_from_db()
+    assert organisation.title == 'Organisation platform title'
+    assert organisation.language == 'de'
+    assert organisation.description == \
         'some very short description of the organisation'
-    assert updated_organisation.language == 'de'
+    assert organisation.slogan == 'some slogan in English'
+    assert organisation.information == 'This is very important info!'
+
+    with switch_language(organisation, 'de'):
+        assert organisation.description == \
+            'Eine sehr kurze Beschreibung der Organisation'
+        assert organisation.slogan == 'Slogan auf Deutsch'
+        assert organisation.information == 'Ein paar wichtige Informationen'
+
+
+@pytest.mark.django_db
+def test_initiator_can_delete_language(client, organisation):
+    initiator = organisation.initiators.first()
+    client.login(username=initiator, password='password')
+    url = reverse('a4dashboard:organisation-settings',
+                  kwargs={'organisation_slug': organisation.slug})
+    response = client.get(url)
+    assert response.status_code == 200
+
+    with switch_language(organisation, 'de'):
+        organisation.description = 'desc.de'
+        organisation.description_why = 'desc why.de'
+        organisation.description_how = 'desc how.de'
+        organisation.save()
+
+    data = {
+        'title': 'Organisation platform title',
+        'language': 'de',
+        'en': 'en',
+        'en__description': 'some very short description of the organisation',
+        'en__slogan': 'some slogan in English',
+        'en__information': 'This is very important info!',
+        'de__description': 'Eine sehr kurze Beschreibung der Organisation',
+        'de__slogan': 'Slogan auf Deutsch',
+        'de__information': 'Ein paar wichtige Informationen',
+    }
+    response = client.post(url, data)
+    assert redirect_target(response) == 'organisation-settings'
+    assert response.status_code == 302
+    organisation.refresh_from_db()
+    organisation.get_translation('en').refresh_from_db()
+    assert organisation.description == \
+        'some very short description of the organisation'
+    assert organisation.slogan == 'some slogan in English'
+    assert organisation.information == 'This is very important info!'
+
+    with switch_language(organisation, 'de'):
+        assert organisation.description == \
+            'some very short description of the organisation'
+        assert organisation.slogan == 'some slogan in English'
+        assert organisation.information == 'This is very important info!'
 
 
 @pytest.mark.django_db
@@ -60,7 +119,13 @@ def test_user_cannot_update(client, organisation, user):
         'title': 'Organisation platform title',
         'language': 'de',
         'en': 'en',
-        'en__description': 'some very short description of the organisation'
+        'en__description': 'some very short description of the organisation',
+        'en__slogan': 'some slogan in English',
+        'en__information': 'This is very important info!',
+        'de': 'de',
+        'de__description': 'Eine sehr kurze Beschreibung der Organisation',
+        'de__slogan': 'Slogan auf Deutsch',
+        'de__information': 'Ein paar wichtige Informationen',
     }
     response = client.post(url, data)
     assert response.status_code == 403
@@ -94,7 +159,13 @@ def test_moderator_cannot_update(client, project):
         'title': 'Organisation platform title',
         'language': 'de',
         'en': 'en',
-        'en__description': 'some very short description of the organisation'
+        'en__description': 'some very short description of the organisation',
+        'en__slogan': 'some slogan in English',
+        'en__information': 'This is very important info!',
+        'de': 'de',
+        'de__description': 'Eine sehr kurze Beschreibung der Organisation',
+        'de__slogan': 'Slogan auf Deutsch',
+        'de__information': 'Ein paar wichtige Informationen',
     }
     response = client.post(url, data)
     assert response.status_code == 403
@@ -129,7 +200,13 @@ def test_member_cannot_update(client, member):
         'title': 'Organisation platform title',
         'language': 'de',
         'en': 'en',
-        'en__description': 'some very short description of the organisation'
+        'en__description': 'some very short description of the organisation',
+        'en__slogan': 'some slogan in English',
+        'en__information': 'This is very important info!',
+        'de': 'de',
+        'de__description': 'Eine sehr kurze Beschreibung der Organisation',
+        'de__slogan': 'Slogan auf Deutsch',
+        'de__information': 'Ein paar wichtige Informationen',
     }
     response = client.post(url, data)
     assert response.status_code == 403
