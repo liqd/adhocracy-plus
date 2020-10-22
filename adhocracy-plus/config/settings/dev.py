124292f83dd135e4bec5f11909381b49544eb03e
Robert Mader <r.mader@liqd.net>
2020-10-22 18:30:28 +0200

    saml2: Add test-server config

--- adhocracy-plus/config/settings/dev.py
+++ adhocracy-plus/config/settings/dev.py
@@ -32,3 +32,48 @@ except NameError:
 BASE_URL = 'http://localhost:8004'
 CAPTCHA_URL = 'https://captcheck.netsyms.com/api.php'
 SITE_ID = 1
+
+# DIID specific test saml2 config
+from os import path
+
+import saml2
+import saml2.saml
+
+BASEDIR = path.dirname(path.abspath(__file__))
+
+SAML_CONFIG = {
+  'entityid': 'http://app.example.com',
+  'allow_unknown_attributes': True,
+  'attribute_map_dir': path.join(BASEDIR, 'saml', 'attribute-maps'),
+  'service': {
+    'sp': {
+      'name': 'Federated Django sample SP',
+      'name_id_format': saml2.saml.NAMEID_FORMAT_PERSISTENT,
+      'endpoints': {
+        'single_logout_service': [
+          ('http://localhost:8004/saml2/ls/', saml2.BINDING_HTTP_REDIRECT),
+          ('http://localhost:8004/saml2/ls/post', saml2.BINDING_HTTP_POST),
+        ],
+        'assertion_consumer_service': [
+          ('http://localhost:8004/saml2/acs/', saml2.BINDING_HTTP_POST),
+        ],
+      },
+      'required_attributes': ['mail'],
+    },
+  },
+  'metadata': {
+    'remote': [{"url": "http://localhost:8080/simplesaml/saml2/idp/metadata.php"},],
+  },
+  'key_file': path.join(BASEDIR, 'saml', 'private.key'),
+  'cert_file': path.join(BASEDIR, 'saml', 'cert.pem'),
+  'encryption_keypairs': [{
+    'key_file': path.join(BASEDIR, 'saml', 'private.key'),
+    'cert_file': path.join(BASEDIR, 'saml', 'cert.pem'),
+  }],
+  'debug': 1,
+}
+SAML_DJANGO_USER_MAIN_ATTRIBUTE = 'email'
+SAML_LOGOUT_REQUEST_PREFERRED_BINDING = saml2.BINDING_HTTP_REDIRECT
+SAML_ATTRIBUTE_MAPPING = {
+    'mail': ['email', 'set_username_from_email'],
+}
