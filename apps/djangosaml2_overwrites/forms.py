a7fac61f8c03642a010e895c1d9903ad42b1ffe8
Robert Mader <robert.mader@posteo.de>
2020-11-03 15:34:41 +0100

    djangosaml2_overwrites: Adapt form to match signup
    And to expose the get_notification option.

--- apps/djangosaml2_overwrites/forms.py
+++ apps/djangosaml2_overwrites/forms.py
@@ -9,10 +9,19 @@ class Djangosaml2SignupForm(forms.ModelForm):
     terms_of_use = forms.BooleanField(
         label=_('Terms of use')
     )
+    data_protection = forms.BooleanField(
+        label=_('Data protection')
+    )
+    get_notifications = forms.BooleanField(
+        label=_('Notifications: Yes, I would like to be notified by e-mail '
+                'about the beginning and end of participation opportunities '
+                'and about comments on my contributions by other users. This '
+                'applies to all projects I follow.'),
+        required=False
+    )
     get_newsletters = forms.BooleanField(
-        label=_('I would like to receive further information'),
-        help_text=_('Projects you are following can send you '
-                    'additional information via email.'),
+        label=_('Newsletter: Yes, I would like to receive e-mail newsletters '
+                'about the projects I follow.'),
         required=False
     )
 
@@ -20,6 +29,7 @@ class Djangosaml2SignupForm(forms.ModelForm):
         model = User
         fields = [
             'username',
+            'get_notifications',
             'get_newsletters'
         ]
 
