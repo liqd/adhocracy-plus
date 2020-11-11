0b9249fb34910fe1f63e413fe91a36e653756fe7
Robert Mader <r.mader@liqd.net>
2020-11-11 15:47:45 +0100

    tests: Remove is_public parameterization from tests
    We explicitly set the projects to the required access level and
    do not seem to evaluate the parameters in any way - so lets remove
    them.

--- tests/mapideas/views/test_mapidea_detail.py
+++ tests/mapideas/views/test_mapidea_detail.py
@@ -32,7 +32,6 @@ def test_detail_view(client, phase_factory, map_idea_factory):
 
 
 @pytest.mark.django_db
-@pytest.mark.parametrized('mapidea__module__project__is_public', [False])
 def test_detail_view_private_not_visible_anonymous(client,
                                                    phase_factory,
                                                    map_idea_factory):
@@ -47,7 +46,6 @@ def test_detail_view_private_not_visible_anonymous(client,
 
 
 @pytest.mark.django_db
-@pytest.mark.parametrized('mapidea__module__project__is_public', [False])
 def test_detail_view_private_not_visible_normal_user(client,
                                                      user,
                                                      phase_factory,
@@ -65,7 +63,6 @@ def test_detail_view_private_not_visible_normal_user(client,
 
 
 @pytest.mark.django_db
-@pytest.mark.parametrized('mapidea__module__project__is_public', [False])
 def test_detail_view_private_visible_to_participant(client,
                                                     user,
                                                     phase_factory,
@@ -84,7 +81,6 @@ def test_detail_view_private_visible_to_participant(client,
 
 
 @pytest.mark.django_db
-@pytest.mark.parametrized('mapidea__module__project__is_public', [False])
 def test_detail_view_private_visible_to_moderator(client,
                                                   phase_factory,
                                                   map_idea_factory):
@@ -101,7 +97,6 @@ def test_detail_view_private_visible_to_moderator(client,
 
 
 @pytest.mark.django_db
-@pytest.mark.parametrized('mapidea__module__project__is_public', [False])
 def test_detail_view_private_visible_to_initiator(client,
                                                   phase_factory,
                                                   map_idea_factory):
