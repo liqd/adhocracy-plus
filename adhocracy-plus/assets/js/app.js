0c295894fa04b43bb6c7844d399f089082aafbb6
Phillippa <p.morland@liqd.net>
2020-11-23 14:38:46 +0100

    base//config: add embed function to ckeditor, add cookie overlay for embedded videos

--- adhocracy-plus/assets/js/app.js
+++ adhocracy-plus/assets/js/app.js
@@ -73,3 +73,9 @@ $(function tooltip () {
 export function getCurrentPath () {
   return location.pathname
 }
+
+// This function adds required classes to iframes added by ckeditor
+$(document).ready(function () {
+  $('.rich-text iframe').addClass('ck_embed_iframe')
+  $('.ck_embed_iframe').parent('div').addClass('ck_embed_iframe__container')
+})
