5ca66c1d232715a0cb7f3bd8f42c6e3d83b75086
Rineee <k.schmid@liqd.net>
2020-12-02 14:01:35 +0100

    apps/interactiveevents: remove old question form, add react question form and do some styling

--- apps/interactiveevents/assets/js/a4_candy_interactive_events/livequestion_filters.jsx
+++ apps/interactiveevents/assets/js/a4_candy_interactive_events/livequestion_filters.jsx
@@ -22,71 +22,69 @@ export default class Filter extends React.Component {
     const displayNotHiddenText = django.gettext('display only questions which are not hidden')
     const orderLikesText = django.gettext('order by likes')
     return (
-      <div className="mb-4">
-        <div className="justify-content-center form-inline">
-          <div className="dropdown mt-3">
-            <button
-              className={this.getButtonClass()} type="button" id="dropdownMenuButton"
-              data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
-            >
-              {this.props.currentCategoryName}
-            </button>
-            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <button className="dropdown-item" data-value={-1} onClick={this.selectCategory.bind(this)} href="#">{allTag}</button>
-              {this.props.categories.map((category, index) => {
-                return <button className="dropdown-item" key={index} data-value={category} onClick={this.selectCategory.bind(this)} href="#">{category}</button>
-              })}
-            </div>
+      <div className="form-inline">
+        <div className="dropdown">
+          <button
+            className={this.getButtonClass()} type="button" id="dropdownMenuButton"
+            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
+          >
+            {this.props.currentCategoryName}
+          </button>
+          <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <button className="dropdown-item" data-value={-1} onClick={this.selectCategory.bind(this)} href="#">{allTag}</button>
+            {this.props.categories.map((category, index) => {
+              return <button className="dropdown-item" key={index} data-value={category} onClick={this.selectCategory.bind(this)} href="#">{category}</button>
+            })}
           </div>
-          {this.props.isModerator &&
-            <div>
-              <div className="checkbox-btn mt-3">
-                <label htmlFor="markedCheck" className="checkbox-btn__label--primary pl-3">
-                  <input
-                    className="checkbox-btn__input"
-                    type="checkbox"
-                    id="markedCheck"
-                    name="markedCheck"
-                    checked={this.props.displayOnShortlist}
-                    onChange={this.props.toggleDisplayOnShortlist} // eslint-disable-line react/jsx-handler-names
-                  />
-                  <span className="checkbox-btn__text">
-                    <i className="icon-in-list" aria-label={onlyShowMarkedText} />
-                  </span>
-                </label>
-              </div>
-              <div className="checkbox-btn mt-3">
-                <label htmlFor="displayNotHiddenOnly" className="checkbox-btn__label--primary pl-3">
-                  <input
-                    className="checkbox-btn__input"
-                    type="checkbox"
-                    id="displayNotHiddenOnly"
-                    name="displayNotHiddenOnly"
-                    checked={this.props.displayNotHiddenOnly}
-                    onChange={this.props.toggledisplayNotHiddenOnly} // eslint-disable-line react/jsx-handler-names
-                  />
-                  <span className="checkbox-btn__text">
-                    <i className="far fa-eye" aria-label={displayNotHiddenText} />
-                  </span>
-                </label>
-              </div>
-              <div className="checkbox-btn mt-3">
-                <label htmlFor="orderedByLikes" className="checkbox-btn__label--primary">
-                  <input
-                    className="checkbox-btn__input"
-                    type="checkbox"
-                    id="orderedByLikes"
-                    name="orderedByLikes"
-                    checked={this.props.orderedByLikes}
-                    onChange={this.props.toggleOrdering} // eslint-disable-line react/jsx-handler-names
-                  />
-                  <span className="checkbox-btn__text">
-                    <i className="icon-like" aria-label={orderLikesText} /> likes
-                  </span>
-                </label>
-              </div>
-            </div>}
         </div>
+        {this.props.isModerator &&
+          <div>
+            <div className="checkbox-btn">
+              <label htmlFor="markedCheck" className="checkbox-btn__label--primary pl-3">
+                <input
+                  className="checkbox-btn__input"
+                  type="checkbox"
+                  id="markedCheck"
+                  name="markedCheck"
+                  checked={this.props.displayOnShortlist}
+                  onChange={this.props.toggleDisplayOnShortlist} // eslint-disable-line react/jsx-handler-names
+                />
+                <span className="checkbox-btn__text">
+                  <i className="icon-in-list" aria-label={onlyShowMarkedText} />
+                </span>
+              </label>
+            </div>
+            <div className="checkbox-btn">
+              <label htmlFor="displayNotHiddenOnly" className="checkbox-btn__label--primary pl-3">
+                <input
+                  className="checkbox-btn__input"
+                  type="checkbox"
+                  id="displayNotHiddenOnly"
+                  name="displayNotHiddenOnly"
+                  checked={this.props.displayNotHiddenOnly}
+                  onChange={this.props.toggledisplayNotHiddenOnly} // eslint-disable-line react/jsx-handler-names
+                />
+                <span className="checkbox-btn__text">
+                  <i className="far fa-eye" aria-label={displayNotHiddenText} />
+                </span>
+              </label>
+            </div>
+            <div className="checkbox-btn">
+              <label htmlFor="orderedByLikes" className="checkbox-btn__label--primary pl-3">
+                <input
+                  className="checkbox-btn__input"
+                  type="checkbox"
+                  id="orderedByLikes"
+                  name="orderedByLikes"
+                  checked={this.props.orderedByLikes}
+                  onChange={this.props.toggleOrdering} // eslint-disable-line react/jsx-handler-names
+                />
+                <span className="checkbox-btn__text">
+                  <i className="icon-like" aria-label={orderLikesText} /> likes
+                </span>
+              </label>
+            </div>
+          </div>}
       </div>
     )
   }
