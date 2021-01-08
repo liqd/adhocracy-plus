f50ac0778cdaf8054753b7045e267ca5b9f500c9
fuzzylogic2000 <katharina.albers@liqd.de>
2021-01-08 11:44:23 +0100

    settings/travis: add new key for deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,18 +5,18 @@ set -e -v
 if [ -n ${TRAVIS_SSH_SECRET} ]; then
     SSH_ID_ARG="-i ${HOME}/id_rsa"
     cat <<EOF | openssl enc -d -aes-256-cbc -pbkdf2 -pass env:TRAVIS_SSH_SECRET -a -out ~/id_rsa
-U2FsdGVkX18u2lLhir31yxnC61lZhVioi12A6W9gjJhjiyETLVQ84/haZhspgDzt
-HFk9V+9JiJGWakrpbH5GexMQDvgzUm4tntw2xU/2LoDBb0iP3HOOYu44Y0VGsTU6
-Wf+vBBE5fOuTpAE0XRla9gEeK9pImhxDDybpgLoPI2x8EqsCaizM/7BtuwfsF5It
-juZB/z3wokkhe0AsQpOk3FPOmzMcW3ILhYHquvzeU4G79muGYNobhan+pv6uvtER
-n7NrFa/oKopa4nJC9Q3fP+itJmTVBaEFuH5vJQWZtSsNvMOPCNq6eXevBInc8aca
-a+6wmHVAp3YI5eWPqmO34c3eo5JSfwJDiJ9tRP6q9w/uR3lkvPq4FVef2ccDfWHe
-jEzDMGdjZJIX0X3WZVQYSnjY3dB4vd8ptHJkdDP2Oj8JTF39CRkYWmNWB68Xa6sf
-7vZ1Q3o3njRGe6R5LrMOQHLR4eMv9f8SjXl8Hifmbk/vqXd9aZD3n75f6ggQR+if
-ywfoxlB+aACB3xQM40xY6LHXeE8YbO1hbpAqF23T72yoLTrPjQ0xi9SvP8oNmsNh
-u9yspubt1Jha/GR03caafw==
+U2FsdGVkX18DO2nMk3k0/oPRl+o9zuQe/XY0LKPQlXbNPIEtKdv43u9fCUqd5xwW
+kS4zYEkLLTHeqKv8H2SH9zQ8IvZyXg7CVmkzXbxXkp1kWU/p/O1CP7eAJr8cdlg/
+TwXwHFw03QsszK764lq8SRTpRxsLJBw5bhO5oDD2/oVD4hZi14IURez1TeRj3+sP
+dPN/Ix25iK8kWFXMDyfbZbNdH3DwN5bP/MqCSAYFe8xZSxLBtHYY+AxaDnkHXvYS
+i9tiejUL3QH9+Lazn5PQFs06Wc+A11TGKkusKv9GbRdF8RzpYsTospjN1H2P61sp
+JV4fgPuVclGgBgRZ2m3Htw/P+PoJDcbP4YiG/ITLIHrg5AnpyPXdOrzVKLUHmsZk
+rRj6kPt3hpGhPoOCTDI0hfB0KGhgM/43OKWy2HMAECC6zGQjh+89FyFer/c+FQsq
+dVmsNHZxMXQJuh4vvOqNvzjq1vxSHajQ6GmlemhHz4rQr7bZB3Xsb7ITw1nvuT4g
+oyXHdRzZlschdUhDs1+4X+5I1o6hL3wK3S/QtdX/dyA6FIzbgHZKFQDIk43DsusV
+Tnc+bZV8UhJN+H+gK0gO4Q==
 EOF
     chmod 600 ~/id_rsa
 fi
 
-ssh ${SSH_ID_ARG} -oStrictHostKeyChecking=no build@build.liqd.net deploy adhocracy-plus master
+ssh ${SSH_ID_ARG} -oStrictHostKeyChecking=no build@build.liqd.net deploy a4_kosmo master
