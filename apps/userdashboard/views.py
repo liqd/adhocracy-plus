ddbf9607b59cd468ed2fe200ca67b3270321850f
Rine <k.schmid@liqd.net>
2021-01-26 17:56:09 +0100

    apps/userdashboard/views: add UserDashboardBaseMixin

--- apps/userdashboard/views.py
+++ apps/userdashboard/views.py
@@ -8,15 +8,11 @@ from apps.organisations.models import Organisation
 from apps.users.models import User
 
 
-class UserDashboardOverviewView(LoginRequiredMixin,
-                                generic.base.ContextMixin,
-                                generic.base.TemplateResponseMixin,
-                                generic.base.View,
-                                ):
-
+class UserDashboardBaseMixin(LoginRequiredMixin,
+                             generic.base.ContextMixin,
+                             generic.base.TemplateResponseMixin,
+                             generic.base.View):
     model = User
-    template_name = 'a4_candy_userdashboard/userdashboard_overview.html'
-    menu_item = 'overview'
 
     def get(self, request):
         response = self.render_to_response(self.get_context_data())
@@ -33,6 +29,12 @@ class UserDashboardOverviewView(LoginRequiredMixin,
     def projects(self):
         return Project.objects.filter(follow__creator=self.request.user,
                                       follow__enabled=True)
+
+
+class UserDashboardOverviewView(UserDashboardBaseMixin):
+
+    template_name = 'a4_candy_userdashboard/userdashboard_overview.html'
+    menu_item = 'overview'
 
     @property
     def actions(self):
@@ -48,41 +50,17 @@ class UserDashboardOverviewView(LoginRequiredMixin,
                 list(sorted_future_projects))[:9]
 
 
-class UserDashboardModerationView(LoginRequiredMixin,
-                                  rules_mixins.PermissionRequiredMixin,
-                                  generic.base.ContextMixin,
-                                  generic.base.TemplateResponseMixin,
-                                  generic.base.View):
+class UserDashboardModerationView(UserDashboardBaseMixin,
+                                  rules_mixins.PermissionRequiredMixin):
 
-    model = User
     template_name = 'a4_candy_userdashboard/userdashboard_moderation.html'
     permission_required = 'a4_candy_userdashboard.view_moderation_dashboard'
     menu_item = 'moderation'
 
-    def get(self, request):
-        response = self.render_to_response(self.get_context_data())
-        return response
 
-    @property
-    def organisations(self):
-        return Organisation.objects.filter(
-            project__follow__creator=self.request.user,
-            project__follow__enabled=True
-        ).distinct()
+class UserDashboardActivitiesView(UserDashboardBaseMixin):
 
-
-class UserDashboardActivitiesView(LoginRequiredMixin,
-                                  generic.base.ContextMixin,
-                                  generic.base.TemplateResponseMixin,
-                                  generic.base.View,
-                                  ):
-
-    model = User
     template_name = 'a4_candy_userdashboard/userdashboard_activities.html'
-
-    def get(self, request):
-        response = self.render_to_response(self.get_context_data())
-        return response
 
     @property
     def actions(self):
@@ -91,20 +69,6 @@ class UserDashboardActivitiesView(LoginRequiredMixin,
         ).exclude_updates()
 
 
-class UserDashboardFollowingView(LoginRequiredMixin,
-                                 generic.base.ContextMixin,
-                                 generic.base.TemplateResponseMixin,
-                                 generic.base.View,
-                                 ):
+class UserDashboardFollowingView(UserDashboardBaseMixin):
 
-    model = User
     template_name = 'a4_candy_userdashboard/userdashboard_following.html'
-
-    def get(self, request):
-        response = self.render_to_response(self.get_context_data())
-        return response
-
-    @property
-    def projects(self):
-        return Project.objects.filter(follow__creator=self.request.user,
-                                      follow__enabled=True)
