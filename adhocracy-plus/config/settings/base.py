a5a3a30b847e30f71588791a7c95e2887a9ed617
Robert Mader <robert.mader@posteo.de>
2021-02-04 12:24:45 +0100

    Add django-samesite-none middleware
    In order to work around certain browsers not assuming this by default.

--- adhocracy-plus/config/settings/base.py
+++ adhocracy-plus/config/settings/base.py
@@ -120,6 +120,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE = (
     'django.middleware.security.SecurityMiddleware',
+    'django_samesite_none.middleware.SameSiteNoneMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
