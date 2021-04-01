3a62e6ebb8666b99c60f039aaed07f3b5d3cbd8e
phillimorland <phillippamorland@gmail.com>
2021-04-01 16:21:06 +0200

    project/serializer//userdashboard/ModerationProjects: adding additional info for mod dashboard and adding it to component

--- apps/projects/serializers.py
+++ apps/projects/serializers.py
@@ -1,3 +1,6 @@
+from functools import lru_cache
+
+from django.utils.translation import ugettext as _
 from easy_thumbnails.files import get_thumbnailer
 from rest_framework import serializers
 
@@ -10,11 +13,47 @@ class ProjectSerializer(serializers.ModelSerializer):
     organisation = serializers.SerializerMethodField()
     tile_image = serializers.SerializerMethodField()
     tile_image_copyright = serializers.SerializerMethodField()
+    status = serializers.SerializerMethodField()
+    participation_active = serializers.SerializerMethodField()
+    participation_string = serializers.SerializerMethodField()
+    future_phase = serializers.SerializerMethodField()
+    active_phase = serializers.SerializerMethodField()
+    past_phase = serializers.SerializerMethodField()
+    offensive = serializers.SerializerMethodField()
+    comment_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Project
         fields = ['title', 'url', 'organisation', 'tile_image',
-                  'tile_image_copyright']
+                  'tile_image_copyright',
+                  'status', 'access',
+                  'participation_active',
+                  'participation_string',
+                  'future_phase', 'active_phase',
+                  'past_phase',
+                  'offensive', 'comment_count']
+
+    @lru_cache(maxsize=1)
+    def _get_participation_status_project(self, instance):
+        project_phases = instance.phases
+
+        if project_phases.active_phases():
+            return _('running'), True
+
+        if project_phases.future_phases():
+            try:
+                return (_('starts on {}').format
+                        (project_phases.future_phases().first().
+                         start_date.strftime('%d.%m.%y')),
+                        True)
+            except AttributeError:
+                return (_('starts in the future'),
+                        True)
+        else:
+            return _('done'), False
+
+    def get_type(self, instance):
+        return 'project'
 
     def get_title(self, instance):
         return instance.name
@@ -42,3 +81,47 @@ class ProjectSerializer(serializers.ModelSerializer):
             return instance.image_copyright
         else:
             return None
+
+    def get_status(self, instance):
+        project_phases = instance.phases
+        if project_phases.active_phases() or project_phases.future_phases():
+            return 0
+        return 1
+
+    def get_participation_active(self, instance):
+        participation_string, participation_active = \
+            self._get_participation_status_project(instance)
+        return participation_active
+
+    def get_participation_string(self, instance):
+        participation_string, participation_active = \
+            self._get_participation_status_project(instance)
+        return str(participation_string)
+
+    def get_future_phase(self, instance):
+        if (instance.future_modules and
+                instance.future_modules.first().module_start):
+            return str(
+                instance.future_modules.first().module_start)
+        return False
+
+    def get_active_phase(self, instance):
+        if instance.active_phase_ends_next:
+            progress = instance.module_running_progress
+            time_left = instance.module_running_time_left
+            end_date = str(instance.running_module_ends_next.module_end)
+            return [progress, time_left, end_date]
+        return False
+
+    def get_past_phase(self, instance):
+        if (instance.past_modules and
+                instance.past_modules.first().module_end):
+            return str(
+                instance.past_modules.first().module_end)
+        return False
+
+    def get_offensive(self, instance):
+        return ('2')
+
+    def get_comment_count(self, instance):
+        return (instance.pk)
--- apps/userdashboard/assets/js/a4_candy_userdashboard/ModerationProjects.jsx
+++ apps/userdashboard/assets/js/a4_candy_userdashboard/ModerationProjects.jsx
@@ -35,13 +35,29 @@ export default class ModerationProjects extends React.Component {
     this.timer = null
   }
 
+  getTimespan (item) {
+    const timeRemaining = item.active_phase[1].split(' ')
+    const daysRemaining = parseInt(timeRemaining[0])
+    if (daysRemaining > 365) {
+      return (
+        <span>{django.gettext('More than 1 year remaining')}</span>
+      )
+    } else {
+      return (
+        <span>{django.gettext('remaining')} {item.active_phase[1]}</span>
+      )
+    }
+  }
+
   render () {
     const { isLoaded, items } = this.state
     const loadingText = django.gettext('Loading...')
+    const byText = django.gettext('By ')
     const commentCountText = django.gettext(' comments')
     const reportCountText = django.gettext(' reports')
-    const projectLengthText = django.gettext(' remaining')
     const srLinkText = django.gettext('Link to ')
+    const privateText = django.gettext('private')
+    const semiPrivateText = django.gettext('semiprivate')
 
     if (!isLoaded) {
       return <div>{loadingText}</div>
@@ -60,18 +76,22 @@ export default class ModerationProjects extends React.Component {
                   </div>
                 </div>
                 <div className="tile__body">
-                  <span className="text-muted">{item.organisation}</span>
+                  <span className="text-muted">{byText}{item.organisation}</span>
                   <a href={item.url}><h3 className="tile__title mb-4">{item.title}</h3></a>
                   <div>
-                    <span className="label label--dark">Project visibility</span>
+                    {item.access === 2 && <span className="label label--dark">{semiPrivateText}</span>}
+                    {item.access === 3 && <span className="label label--dark">{privateText}</span>}
                   </div>
-                  <div className="d-flex justify-content-between text-muted mt-3">
-                    <span><span className="fa-stack fa-2x" aria-hidden="true"><i className="fas fa-exclamation fa-stack-1x" /><i className="far fa-circle fa-stack-2x" /></span>{reportCountText}</span>
-                    <span><i className="far fa-comment" aria-hidden="true" />{commentCountText}</span>
-                    <span><i className="far fa-clock" aria-hidden="true" />{projectLengthText}</span>
+                  <div className="row text-muted mt-3">
+                    {item.offensive > 0 && <div className="col-4"><span className="fa-stack fa-2x" aria-hidden="true"><i className="fas fa-exclamation fa-stack-1x" /><i className="far fa-circle fa-stack-2x" /></span> {item.offensive}{reportCountText}</div>}
+                    {item.comment_count > 0 && <div className="col-4"><i className="far fa-comment" aria-hidden="true" /> {item.comment_count}{commentCountText}</div>}
+                    {item.future_phase && !item.active_phase && <div className="col-4"><i className="far fa-clock" aria-hidden="true" /> {item.participation_string}</div>}
+                    {item.active_phase && <div className="col-4"><i className="far fa-clock" aria-hidden="true" /> {this.getTimespan(item)}</div>}
+                    {item.past_phase && !item.active_phase && !item.future_phase && <div className="col-4" />}
                   </div>
                   <a href={item.url} className="tile__link"><span className="sr-only">{srLinkText}{item.title}</span></a>
                 </div>
+
               </li>
             ))}
           </ul>
