b61cce173daae3917096da66de2e0564b3c7e8b2
Rine <k.schmid@liqd.net>
2021-04-09 18:21:16 +0200

    apps/classification: call ai api every time a comment is saved and store offensive classifications

--- apps/classifications/apps.py
+++ apps/classifications/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class Config(AppConfig):
     name = 'apps.classifications'
     label = 'a4_candy_classifications'
+
+    def ready(self):
+        import apps.classifications.signals  # noqa:F401
---
+++ apps/classifications/signals.py
@@ -0,0 +1,51 @@
+import logging
+
+import backoff
+import httpx
+from django.conf import settings
+from django.db.models import signals
+from django.dispatch import receiver
+
+from adhocracy4.comments.models import Comment
+
+from .models import AIClassification
+
+client = httpx.Client()
+logger = logging.getLogger(__name__)
+
+
+@receiver(signals.post_save, sender=Comment)
+def get_ai_classification(sender, instance, **kwargs):
+    try:
+        response = call_ai_api(comment=instance)
+        if response.status_code == 200 \
+                and response.json()['classification'] == 'OFFENSE':
+
+            classification = AIClassification(
+                comment=instance,
+                classification='OFFENSIVE')
+            classification.save()
+    except httpx.HTTPError:
+        logger.error('Error connecting to %s', settings.AI_API_URL)
+
+
+def skip_retry(e):
+    if isinstance(e, httpx.HTTPStatusError):
+        return 400 <= e.response.status_code < 500
+    return False
+
+
+@backoff.on_exception(backoff.expo,
+                      httpx.HTTPError,
+                      max_tries=4,
+                      factor=2,
+                      giveup=skip_retry)
+def call_ai_api(comment):
+    response = client.post(settings.AI_API_URL,
+                           data={'comment': comment.comment},
+                           headers={'Authorization': 'Token {}'.format(
+                                    settings.AI_API_AUTH_TOKEN),
+                                    'Accept': 'application/json; version=2.0'})
+    response.raise_for_status()
+
+    return response
