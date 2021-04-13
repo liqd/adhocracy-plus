06b3ab0adee570156c82809b31b5fbd89d1fc043
Rine <k.schmid@liqd.net>
2021-04-13 18:51:20 +0200

    tests/classifications: add tests for signals

---
+++ tests/classifications/test_classification_signals.py
@@ -0,0 +1,15 @@
+import pytest
+from django.db.models import signals
+
+from adhocracy4.comments.models import Comment
+from apps.classifications.signals import get_ai_classification
+
+
+@pytest.mark.django_db
+def test_ai_classification_receives_post_save_comment_signal(idea,
+                                                             comment_factory,
+                                                             caplog):
+    assert(get_ai_classification in signals.post_save._live_receivers(Comment))
+    comment_factory(content_object=idea,
+                    comment='lala')
+    assert('Error connecting to %s' in str(caplog.records[-1]))
