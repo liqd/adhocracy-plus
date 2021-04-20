981b7363076472f147f1f8e63618048166ceb3ac
Rine <k.schmid@liqd.net>
2021-04-20 19:04:42 +0200

    projects/api: return list of moderatrion projects sorted by number of classifications

--- apps/projects/api.py
+++ apps/projects/api.py
@@ -2,9 +2,16 @@ from rest_framework import viewsets
 
 from apps.projects.serializers import ProjectSerializer
 
+from . import helpers
+
 
 class ModerationProjectsViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = ProjectSerializer
 
     def get_queryset(self):
-        return list(self.request.user.project_moderator.all())
+        project_offensive_count = \
+            {project: helpers.get_num_classifications(project)
+             for project in list(self.request.user.project_moderator.all())}
+        return sorted(project_offensive_count,
+                      key=project_offensive_count.get,
+                      reverse=True)
