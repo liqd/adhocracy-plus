3e44cf57c1727caf9b9875fa7a02f0c0ba030a35
Rine <k.schmid@liqd.net>
2021-05-03 11:54:56 +0200

    tests/interactiveevents: adjust to new ExtraFieldsInteractiveEvent model

--- tests/interactiveevents/views/test_interactiveevent_livequestion_detail.py
+++ tests/interactiveevents/views/test_interactiveevent_livequestion_detail.py
@@ -10,7 +10,7 @@ from tests.helpers import setup_phase
 
 @pytest.mark.django_db
 def test_detail_view(client, user, phase_factory, live_question_factory,
-                     live_stream_factory):
+                     interactive_extra_fields_factory):
     phase, module, project, live_question = setup_phase(
         phase_factory, live_question_factory, phases.IssuePhase,
         start_date=parse('2013-01-01 17:00:00 UTC'),
@@ -29,11 +29,12 @@ def test_detail_view(client, user, phase_factory, live_question_factory,
             'a4_candy_interactive_events/livequestion_module_detail.html')
         assert 'Live now' not in response.content.decode()
 
-    live_stream = live_stream_factory(module=module, creator_id=user.id)
-    live_stream.live_stream = \
+    interactive_extra_fields = \
+        interactive_extra_fields_factory(module=module, creator_id=user.id)
+    interactive_extra_fields.live_stream = \
         '<div><div><iframe src="https://some.video" style=""></iframe>' \
         '</div></div>'
-    live_stream.save()
+    interactive_extra_fields.save()
 
     with freeze_time(parse('2013-01-01 18:00:00 UTC')):
         response = client.get(url)
