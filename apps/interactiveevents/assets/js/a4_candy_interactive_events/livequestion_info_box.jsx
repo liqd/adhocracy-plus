e63082df6f67a93348572575c3059d268e7fcf78
phillimorland <phillippamorland@gmail.com>
2021-05-06 10:33:29 +0200

    apps/livequestions: update structure and some functionality of live event with styling

--- apps/interactiveevents/assets/js/a4_candy_interactive_events/livequestion_info_box.jsx
+++ apps/interactiveevents/assets/js/a4_candy_interactive_events/livequestion_info_box.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import django from 'django'
 
+const textInfo = django.gettext('Info')
 const textDisplayQuestion = django.gettext('display question on screen')
 const textAddQuestion = django.gettext('add question to shortlist')
 const textHideQuestion = django.gettext('hide question from audience')
@@ -27,45 +28,37 @@ export default class InfoBox extends React.Component {
 
   render () {
     return (
-      <div>
-        {this.state.displayInfo
-          ? (
-            <div className="alert alert--success alert-dismissible my-2">
-              {this.props.isModerator &&
-                <div className="row pt-4">
-                  <div className="col-lg-3 pb-2 pb-xl-0">
-                    <i className="icon-push-in-list" /> <span>{textAddQuestion}</span>
-                  </div>
-                  <div className="col-lg-3 pb-2 pb-xl-0">
-                    <span className="fa-stack fa-1x"><i className="fas fa-tv fa-stack-2x" /><i className="fas fa-arrow-up fa-stack-1x" /></span> <span>{textDisplayQuestion}</span>
-                  </div>
-                  <div className="col-lg-3 pb-2 pb-xl-0">
-                    <i className="icon-answered" /> <span>{textMarkAnswered}</span>
-                  </div>
-                  <div className="col-lg-3 pb-2 pb-xl-0">
-                    <i className="far fa-eye" /> <span>{textHideQuestion}</span>
-                  </div>
-                </div>}
-              {!this.props.isModerator &&
-                <div className="row mb-2">
-                  <div className="col-12">
-                    <i className="icon-in-list" /> {textMarkedModeration}
-                  </div>
-                </div>}
-              <button type="button" className="close" onClick={this.toggleInformation.bind(this)}>
-                <span aria-label={ariaCloseInfo}>&times;</span>
-              </button>
-            </div>
-            )
-          : (
-            <div className="row mb-2">
-              <div className="col-12 d-flex justify-content-end">
-                <button type="button" className="btn btn--primary" onClick={this.toggleInformation.bind(this)}>
-                  <span aria-label={ariaOpenInfo}><i className="fas fa-info-circle" /></span>
-                </button>
-              </div>
-            </div>
-            )}
+      <div className="livequestion__info-box">
+        {this.state.displayInfo &&
+          <div className="u-border my-2">
+            <button type="button" className="close pr-2" onClick={this.toggleInformation.bind(this)}>
+              <span aria-label={ariaCloseInfo}>&times;</span>
+            </button>
+            {this.props.isModerator &&
+              <div className="row pt-4">
+                <div className="col-lg-3 text-center">
+                  <i className="icon-push-in-list" /> <div>{textAddQuestion}</div>
+                </div>
+                <div className="col-lg-3 text-center">
+                  <span className="fa-stack fa-1x"><i className="fas fa-tv fa-stack-2x" /><i className="fas fa-arrow-up fa-stack-1x" /></span> <div>{textDisplayQuestion}</div>
+                </div>
+                <div className="col-lg-3 text-center">
+                  <i className="icon-answered" /> <div>{textMarkAnswered}</div>
+                </div>
+                <div className="col-lg-3 text-center pb-3">
+                  <i className="far fa-eye" /> <div>{textHideQuestion}</div>
+                </div>
+              </div>}
+            {!this.props.isModerator &&
+              <div className="row pt-4">
+                <div className="col-12 text-center">
+                  <i className="icon-in-list" /> <div>{textMarkedModeration}</div>
+                </div>
+              </div>}
+          </div>}
+        <button type="button" className="btn btn--none text-muted pt-0 pr-0" onClick={this.toggleInformation.bind(this)}>
+          <span aria-label={ariaOpenInfo}><i className="fas fa-info-circle mr-1" />{textInfo}</span>
+        </button>
       </div>
     )
   }
