8344e2c3f6c2d319eb61570ddc2ad70a50aaec34
phillimorland <phillippamorland@gmail.com>
2021-05-20 11:19:43 +0200

    package.json: rearrange dependencies

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,6 @@ const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
-  // controls source mapping to assist in debugging
   mode: 'development',
-  devtool: 'eval'
+  devtool: 'eval' // controls source mapping to assist in debugging
 })
