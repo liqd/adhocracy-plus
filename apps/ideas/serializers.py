e995a886791d8db000969c13fa2a2c216a3ec8e6
Rine <k.schmid@liqd.net>
2021-05-25 18:37:07 +0200

    apps/ideas/api//serializers: add comment and rating count to serializers

--- apps/ideas/serializers.py
+++ apps/ideas/serializers.py
@@ -6,11 +6,25 @@ from .models import Idea
 class IdeaSerializer(serializers.ModelSerializer):
 
     creator = serializers.SerializerMethodField()
+    comment_count = serializers.SerializerMethodField()
+    positive_rating_count = serializers.SerializerMethodField()
+    negative_rating_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Idea
-        fields = ('name', 'description', 'creator', 'created')
+        fields = ('pk', 'name', 'description', 'creator', 'created',
+                  'comment_count', 'positive_rating_count',
+                  'negative_rating_count')
         read_only_fields = ('creator', 'created')
 
     def get_creator(self, idea):
         return idea.creator.username
+
+    def get_comment_count(self, idea):
+        return idea.comment_count
+
+    def get_positive_rating_count(self, idea):
+        return idea.positive_rating_count
+
+    def get_negative_rating_count(self, idea):
+        return idea.negative_rating_count
