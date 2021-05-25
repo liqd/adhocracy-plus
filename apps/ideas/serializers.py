2477264c1ef4bf60f0adc4bb14b3489140bbf564
Rine <k.schmid@liqd.net>
2021-05-25 11:16:16 +0200

    ideas/serializers: add creator and created to serializer

--- apps/ideas/serializers.py
+++ apps/ideas/serializers.py
@@ -5,6 +5,12 @@ from .models import Idea
 
 class IdeaSerializer(serializers.ModelSerializer):
 
+    creator = serializers.SerializerMethodField()
+
     class Meta:
         model = Idea
-        fields = ('name', 'description')
+        fields = ('name', 'description', 'creator', 'created')
+        read_only_fields = ('creator', 'created')
+
+    def get_creator(self, idea):
+        return idea.creator.username
