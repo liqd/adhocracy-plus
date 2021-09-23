ba7221f9505f87883784b7cbd7114fdbf1f43f82
rine <k.schmid@liqd.net>
2021-09-23 19:08:51 +0200

    apps/exports//polls: remove poll dashboard components, exports, views and basic DashboardExportView and export template

--- apps/polls/dashboard.py
+++ apps/polls/dashboard.py
@@ -1,15 +1,13 @@
 from django.urls import reverse
-from django.utils.translation import ugettext_lazy as _
 
-from adhocracy4.dashboard import DashboardComponent
 from adhocracy4.dashboard import components
-from adhocracy4.polls.dashboard import PollComponent as A4PollComponent
+from adhocracy4.polls import dashboard as a4_poll_dashboard
+from adhocracy4.polls import exports as a4_poll_exports
 
-from . import exports
 from . import views
 
 
-class PollComponent(A4PollComponent):
+class PollComponent(a4_poll_dashboard.PollComponent):
 
     def get_base_url(self, module):
         return reverse('a4dashboard:poll-dashboard', kwargs={
@@ -18,21 +16,7 @@ class PollComponent(A4PollComponent):
         })
 
 
-components.replace_module(PollComponent())
-
-
-class ExportPollComponent(DashboardComponent):
-    identifier = 'poll_export'
-    weight = 50
-    label = _('Export Excel')
-
-    def is_effective(self, module):
-        module_app = module.phases[0].content().app
-        return (module_app == 'a4polls' and
-                not module.project.is_draft and not module.is_draft)
-
-    def get_progress(self, module):
-        return 0, 0
+class ExportPollComponent(a4_poll_dashboard.ExportPollComponent):
 
     def get_base_url(self, module):
         return reverse('a4dashboard:poll-export-module', kwargs={
@@ -46,9 +30,13 @@ class ExportPollComponent(DashboardComponent):
              views.PollDashboardExportView.as_view(),
              'poll-export-module'),
             (r'^modules/(?P<module_slug>[-\w_]+)/poll/export/comments/$',
-             exports.PollCommentExportView.as_view(),
+             a4_poll_exports.PollCommentExportView.as_view(),
              'poll-comment-export'),
+            (r'^modules/(?P<module_slug>[-\w_]+)/poll/export/poll/$',
+             a4_poll_exports.PollExportView.as_view(),
+             'poll-export'),
         ]
 
 
-components.register_module(ExportPollComponent())
+components.replace_module(PollComponent())
+components.replace_module(ExportPollComponent())
