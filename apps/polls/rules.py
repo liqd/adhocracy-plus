08ec5fc32494f4745dbb73117a5b0a4a58e14145
rine <k.schmid@liqd.net>
2021-09-30 14:46:20 +0200

    apps/polls/rules: fix overwrite/use right permission

--- apps/polls/rules.py
+++ apps/polls/rules.py
@@ -2,8 +2,9 @@ import rules
 
 from adhocracy4.modules import predicates as module_predicates
 
+rules.remove_perm('a4polls.change_poll')
 rules.add_perm(
-    'a4_candy_polls.change_poll',
+    'a4polls.change_poll',
     module_predicates.is_context_initiator |
     module_predicates.is_context_moderator
 )
