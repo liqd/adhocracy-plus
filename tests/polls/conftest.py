87cb04e3264d55f4e8ff836ec109c9daa6fd475d
rine <k.schmid@liqd.net>
2021-09-30 14:47:40 +0200

    tests/polls: only test overwrites and remove other tests that are already in A4

--- tests/polls/conftest.py
+++ tests/polls/conftest.py
@@ -3,6 +3,3 @@ from pytest_factoryboy import register
 from adhocracy4.test.factories import polls as factories
 
 register(factories.PollFactory)
-register(factories.QuestionFactory)
-register(factories.ChoiceFactory)
-register(factories.VoteFactory)
