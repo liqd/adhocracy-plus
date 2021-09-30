3de0c7e32b9131eaa15fca10635bb78ebe84bfff
rine <k.schmid@liqd.net>
2021-09-30 14:47:40 +0200

    tests/polls: only test overwrites and remove other tests that are already in A4

--- tests/polls/dashboard_components/test_views_module_polls.py
+++ tests/polls/dashboard_components/test_views_module_polls.py
@@ -1,11 +1,13 @@
 import pytest
+from django.urls import reverse
 
 from adhocracy4.dashboard import components
 from adhocracy4.polls.phases import VotingPhase
 from adhocracy4.test.helpers import assert_template_response
 from adhocracy4.test.helpers import setup_phase
 
-component = components.modules.get('polls')
+poll_component = components.modules.get('polls')
+export_poll_component = components.modules.get('poll_export')
 
 
 @pytest.mark.django_db
@@ -13,8 +15,41 @@ def test_edit_view(client, phase_factory):
     phase, module, project, item = setup_phase(
         phase_factory, None, VotingPhase)
     initiator = module.project.organisation.initiators.first()
-    url = component.get_base_url(module)
+    url = poll_component.get_base_url(module)
     client.login(username=initiator.email, password='password')
     response = client.get(url)
     assert_template_response(
         response, 'a4polls/poll_dashboard.html')
+
+
+@pytest.mark.django_db
+def test_poll_dashboard_export_view(client, phase_factory, user_factory):
+    phase, module, project, item = setup_phase(
+        phase_factory, None, VotingPhase)
+    user = user_factory()
+    initiator = user_factory()
+    project.organisation.initiators.add(initiator)
+    url = export_poll_component.get_base_url(module)
+
+    client.login(username=user.username, password='password')
+    response = client.get(url)
+    assert response.status_code == 403
+
+    client.login(username=initiator.username, password='password')
+    response = client.get(url)
+    assert_template_response(response, 'a4exports/export_dashboard.html')
+    assert response.template_name[0] == 'a4exports/export_dashboard.html'
+    assert 'poll_export' in response.context
+    assert response.context['poll_export'] == \
+           reverse('a4dashboard:poll-export',
+                   kwargs={
+                       'organisation_slug': module.project.organisation.slug,
+                       'module_slug': module.slug
+                   })
+    assert 'comment_export' in response.context
+    assert response.context['comment_export'] == \
+           reverse('a4dashboard:poll-comment-export',
+                   kwargs={
+                       'organisation_slug': module.project.organisation.slug,
+                       'module_slug': module.slug
+                   })
