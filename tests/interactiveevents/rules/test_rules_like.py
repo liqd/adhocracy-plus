b1a009db365ae5cebbd17f20e00fd7eef9ba4e3d
fuzzylogic2000 <k.lindenlaub@liqd.net>
2022-01-11 09:44:17 +0100

    tests/interactivevents: add tests for predicates and rules

--- tests/interactiveevents/rules/test_rules_like.py
+++ tests/interactiveevents/rules/test_rules_like.py
@@ -8,7 +8,7 @@ from adhocracy4.test.helpers import setup_phase
 from adhocracy4.test.helpers import setup_users
 from apps.interactiveevents import phases
 
-perm_name = 'a4_candy_interactive_events.add_like_model'
+perm_name = 'a4_candy_interactive_events.add_like'
 
 
 def test_perm_exists():
@@ -17,46 +17,78 @@ def test_perm_exists():
 
 @pytest.mark.django_db
 def test_pre_phase(phase_factory, live_question_factory, user):
-    phase, module, project, item = setup_phase(phase_factory,
-                                               live_question_factory,
-                                               phases.IssuePhase)
+    phase, _, project, item = setup_phase(phase_factory, live_question_factory,
+                                          phases.IssuePhase)
     anonymous, moderator, initiator = setup_users(project)
 
     assert project.is_public
     with freeze_pre_phase(phase):
-        assert not rules.has_perm(perm_name, anonymous, module)
-        assert not rules.has_perm(perm_name, user, module)
-        assert not rules.has_perm(perm_name, moderator, module)
-        assert not rules.has_perm(perm_name, initiator, module)
+        assert not rules.has_perm(perm_name, anonymous, item)
+        assert not rules.has_perm(perm_name, user, item)
+        assert not rules.has_perm(perm_name, moderator, item)
+        assert not rules.has_perm(perm_name, initiator, item)
 
 
 @pytest.mark.django_db
 def test_phase_active(phase_factory, live_question_factory, user):
-    phase, module, project, item = setup_phase(phase_factory,
-                                               live_question_factory,
-                                               phases.IssuePhase)
+    phase, _, project, item = setup_phase(phase_factory, live_question_factory,
+                                          phases.IssuePhase)
     anonymous, moderator, initiator = setup_users(project)
 
     assert project.is_public
     with freeze_phase(phase):
-        assert rules.has_perm(perm_name, anonymous, module)
-        assert rules.has_perm(perm_name, user, module)
-        assert rules.has_perm(perm_name, moderator, module)
-        assert rules.has_perm(perm_name, initiator, module)
+        assert rules.has_perm(perm_name, anonymous, item)
+        assert rules.has_perm(perm_name, user, item)
+        assert rules.has_perm(perm_name, moderator, item)
+        assert rules.has_perm(perm_name, initiator, item)
+
+
+@pytest.mark.django_db
+def test_post_phase(phase_factory, live_question_factory, user):
+    phase, _, project, item = setup_phase(phase_factory,
+                                          live_question_factory,
+                                          phases.IssuePhase)
+    anonymous, moderator, initiator = setup_users(project)
+
+    assert project.is_public
+    with freeze_post_phase(phase):
+        assert not rules.has_perm(perm_name, anonymous, item)
+        assert not rules.has_perm(perm_name, user, item)
+        assert not rules.has_perm(perm_name, moderator, item)
+        assert not rules.has_perm(perm_name, initiator, item)
+
+
+@pytest.mark.django_db
+def test_phase_active_project_draft(phase_factory,
+                                    live_question_factory,
+                                    user):
+    phase, _, project, item = setup_phase(phase_factory,
+                                          live_question_factory,
+                                          phases.IssuePhase,
+                                          module__project__is_draft=True)
+    anonymous, moderator, initiator = setup_users(project)
+
+    assert project.is_draft
+    with freeze_phase(phase):
+        assert rules.has_perm(perm_name, anonymous, item)
+        assert rules.has_perm(perm_name, user, item)
+        assert rules.has_perm(perm_name, moderator, item)
+        assert rules.has_perm(perm_name, initiator, item)
 
 
 @pytest.mark.django_db
 def test_post_phase_project_archived(phase_factory,
                                      live_question_factory,
                                      user):
-    phase, module, project, item = setup_phase(
-        phase_factory, live_question_factory, phases.IssuePhase,
-        module__project__is_archived=True)
+    phase, _, project, item = setup_phase(phase_factory,
+                                          live_question_factory,
+                                          phases.IssuePhase,
+                                          module__project__is_archived=True)
     anonymous, moderator, initiator = setup_users(project)
 
     assert project.is_archived
     with freeze_post_phase(phase):
-        assert not rules.has_perm(perm_name, anonymous, module)
-        assert not rules.has_perm(perm_name, user, module)
-        assert not rules.has_perm(perm_name, moderator, module)
-        assert not rules.has_perm(perm_name, initiator, module)
+        assert not rules.has_perm(perm_name, anonymous, item)
+        assert not rules.has_perm(perm_name, user, item)
+        assert not rules.has_perm(perm_name, moderator, item)
+        assert not rules.has_perm(perm_name, initiator, item)
---
+++ tests/interactiveevents/test_interactiveevent_predicates.py
@@ -0,0 +1,66 @@
+import pytest
+
+from adhocracy4.test.helpers import freeze_phase
+from adhocracy4.test.helpers import freeze_post_phase
+from adhocracy4.test.helpers import setup_phase
+from apps.interactiveevents import phases
+from apps.interactiveevents import predicates
+from apps.interactiveevents.models import LiveQuestion
+
+
+@pytest.mark.django_db
+def test_phase_allows_like_active(user, admin, phase_factory,
+                                  live_question_factory):
+    phase, _, _, livequestion = setup_phase(
+        phase_factory, live_question_factory, phases.IssuePhase
+    )
+
+    with freeze_phase(phase):
+        assert not predicates.phase_allows_like(user, False)
+        assert predicates.phase_allows_like(user, livequestion)
+        assert predicates.phase_allows_like(admin, livequestion)
+
+
+@pytest.mark.django_db
+def test_phase_allows_like_post(user, admin, phase_factory,
+                                live_question_factory):
+    phase, _, _, livequestion = setup_phase(
+        phase_factory, live_question_factory, phases.IssuePhase
+    )
+
+    with freeze_post_phase(phase):
+        assert not predicates.phase_allows_like(user, False)
+        assert not predicates.phase_allows_like(user, livequestion)
+        assert not predicates.phase_allows_like(admin, livequestion)
+
+
+@pytest.mark.django_db
+def test_phase_allows_like_model_active(user, admin, phase_factory,
+                                        live_question_factory):
+    phase, module, _, livequestion = setup_phase(
+        phase_factory, live_question_factory, phases.IssuePhase
+    )
+
+    with freeze_phase(phase):
+        assert not predicates.phase_allows_like_model(LiveQuestion)(user,
+                                                                    False)
+        assert predicates.phase_allows_like_model(LiveQuestion)(user,
+                                                                module)
+        assert predicates.phase_allows_like_model(LiveQuestion)(admin,
+                                                                module)
+
+
+@pytest.mark.django_db
+def test_phase_allows_like_model_post(user, admin, phase_factory,
+                                      live_question_factory):
+    phase, module, _, _ = setup_phase(
+        phase_factory, live_question_factory, phases.IssuePhase
+    )
+
+    with freeze_post_phase(phase):
+        assert not predicates.phase_allows_like_model(LiveQuestion)(user,
+                                                                    False)
+        assert not predicates.phase_allows_like_model(LiveQuestion)(user,
+                                                                    module)
+        assert not predicates.phase_allows_like_model(LiveQuestion)(admin,
+                                                                    module)
