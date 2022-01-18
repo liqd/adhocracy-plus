6c9be35bb186c8cd504f492255f866dd224ff95c
fuzzylogic2000 <k.lindenlaub@liqd.net>
2022-01-18 11:28:30 +0100

    django-admin: fix custon CK editor fields - fixes #1656

--- apps/organisations/admin.py
+++ apps/organisations/admin.py
@@ -1,5 +1,7 @@
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from django.contrib import admin
 from parler.admin import TranslatableAdmin
+from parler.forms import TranslatableModelForm
 
 from . import models
 
@@ -13,8 +15,22 @@ class MemberInline(admin.TabularInline):
     extra = 1
 
 
+class OrganisationAdminForm(TranslatableModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['information'].widget = CKEditorUploadingWidget(
+            config_name='collapsible-image-editor')
+
+    class Meta:
+        model = models.Organisation
+        fields = '__all__'
+
+
 @admin.register(models.Organisation)
 class OrganisationAdmin(TranslatableAdmin):
+    form = OrganisationAdminForm
     search_fields = ('name', 'slug')
     list_display = (
         'id', 'name', 'slug', 'site'
@@ -23,6 +39,31 @@ class OrganisationAdmin(TranslatableAdmin):
     inlines = [
         MemberInline,
     ]
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'initiators', 'title')
+        }),
+        ('Translations', {
+            'fields': ('description', 'slogan', 'information'),
+        }),
+        ('Images', {
+            'classes': ('collapse',),
+            'fields': ('logo', 'image', 'image_copyright'),
+        }),
+        ('Website and social media', {
+            'classes': ('collapse',),
+            'fields': ('url', 'twitter_handle', 'facebook_handle',
+                       'instagram_handle'),
+        }),
+        ('Legal information', {
+            'classes': ('collapse',),
+            'fields': ('imprint', 'terms_of_use', 'data_protection',
+                       'netiquette'),
+        }),
+        ('Settings', {
+            'fields': ('is_supporting', 'language', 'site'),
+        }),
+    )
 
 
 @admin.register(models.Member)
