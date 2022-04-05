4d0782219996ae54e8b9daeed358857ba8c753a0
sabinammm <isobel@posteo.de>
2022-04-05 10:35:57 +0200

    apps/rules: only owner can edit comment

---
+++ apps/contrib/rules.py
@@ -0,0 +1,15 @@
+import rules
+from rules.predicates import is_superuser
+
+from adhocracy4.comments.rules import content_object_allows_comment
+from adhocracy4.modules import predicates as module_predicates
+
+rules.set_perm(
+    'a4comments.change_comment',
+    is_superuser |
+    (
+        module_predicates.is_context_member &
+        content_object_allows_comment &
+        module_predicates.is_owner
+    )
+)
