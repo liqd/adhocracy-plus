308aa8c5de9aa251e35b3f71f0b6540423d1686a
sabinammm <isobel@posteo.de>
2022-04-08 10:33:15 +0200

    tests/comments: test rules and api

---
+++ tests/comments/rules/test_rules_change_comment.py
@@ -0,0 +1,101 @@
+import pytest
+import rules
+
+from adhocracy4.test.helpers import freeze_phase
+from adhocracy4.test.helpers import freeze_post_phase
+from adhocracy4.test.helpers import freeze_pre_phase
+from adhocracy4.test.helpers import setup_phase
+from adhocracy4.test.helpers import setup_users
+from apps.ideas import phases
+
+perm_name = 'a4comments.change_comment'
+
+
+def test_perm_exists():
+    assert rules.perm_exists(perm_name)
+
+
+@pytest.mark.django_db
+def test_pre_phase(phase_factory, idea_factory, user, member_factory,
+                   admin, comment_factory):
+    phase, _, project, item = setup_phase(phase_factory, idea_factory,
+                                          phases.CollectPhase)
+    anonymous, moderator, initiator = setup_users(project)
+    comment = comment_factory(content_object=item)
+    creator = comment.creator
+    member = member_factory(organisation=project.organisation)
+
+    assert project.is_public
+    with freeze_pre_phase(phase):
+        assert not rules.has_perm(perm_name, anonymous, comment)
+        assert not rules.has_perm(perm_name, user, comment)
+        assert not rules.has_perm(perm_name, member.member, comment)
+        assert not rules.has_perm(perm_name, creator, comment)
+        assert rules.has_perm(perm_name, admin, comment)
+        assert not rules.has_perm(perm_name, moderator, comment)
+        assert not rules.has_perm(perm_name, initiator, comment)
+
+
+@pytest.mark.django_db
+def test_phase_active(phase_factory, idea_factory, user,
+                      member_factory, admin, comment_factory):
+    phase, _, project, item = setup_phase(phase_factory, idea_factory,
+                                          phases.CollectPhase)
+    anonymous, moderator, initiator = setup_users(project)
+    comment = comment_factory(content_object=item)
+    creator = comment.creator
+    member = member_factory(organisation=project.organisation)
+
+    assert project.is_public
+    with freeze_phase(phase):
+        assert not rules.has_perm(perm_name, anonymous, comment)
+        assert not rules.has_perm(perm_name, user, comment)
+        assert not rules.has_perm(perm_name, member.member, comment)
+        assert rules.has_perm(perm_name, creator, comment)
+        assert rules.has_perm(perm_name, admin, comment)
+        assert not rules.has_perm(perm_name, moderator, comment)
+        assert not rules.has_perm(perm_name, initiator, comment)
+
+
+@pytest.mark.django_db
+def test_phase_active_project_draft(phase_factory, idea_factory, user,
+                                    member_factory, admin, comment_factory):
+    phase, _, project, item = setup_phase(phase_factory, idea_factory,
+                                          phases.RatingPhase,
+                                          module__project__is_draft=True)
+    anonymous, moderator, initiator = setup_users(project)
+    comment = comment_factory(content_object=item)
+    creator = comment.creator
+    member = member_factory(organisation=project.organisation)
+
+    assert project.is_draft
+    with freeze_phase(phase):
+        assert not rules.has_perm(perm_name, anonymous, comment)
+        assert not rules.has_perm(perm_name, user, comment)
+        assert not rules.has_perm(perm_name, member.member, comment)
+        assert not rules.has_perm(perm_name, creator, comment)
+        assert rules.has_perm(perm_name, admin, comment)
+        assert not rules.has_perm(perm_name, moderator, comment)
+        assert not rules.has_perm(perm_name, initiator, comment)
+
+
+@pytest.mark.django_db
+def test_post_phase_project_archived(phase_factory, idea_factory, user,
+                                     member_factory, admin, comment_factory):
+    phase, _, project, item = setup_phase(phase_factory, idea_factory,
+                                          phases.CollectPhase,
+                                          module__project__is_archived=True)
+    anonymous, moderator, initiator = setup_users(project)
+    comment = comment_factory(content_object=item)
+    creator = comment.creator
+    member = member_factory(organisation=project.organisation)
+
+    assert project.is_archived
+    with freeze_post_phase(phase):
+        assert not rules.has_perm(perm_name, anonymous, comment)
+        assert not rules.has_perm(perm_name, user, comment)
+        assert not rules.has_perm(perm_name, member.member, comment)
+        assert not rules.has_perm(perm_name, creator, comment)
+        assert rules.has_perm(perm_name, admin, comment)
+        assert not rules.has_perm(perm_name, moderator, comment)
+        assert not rules.has_perm(perm_name, initiator, comment)
---
+++ tests/comments/test_async_api.py
@@ -0,0 +1,200 @@
+import pytest
+from django.urls import reverse
+from rest_framework import status
+
+from adhocracy4.test.helpers import freeze_phase
+from adhocracy4.test.helpers import setup_phase
+from adhocracy4.test.helpers import setup_users
+from apps.ideas.phases import CollectPhase
+from apps.ideas.phases import RatingPhase
+
+
+@pytest.mark.django_db
+def test_authenticated_user_can_edit_own_comment(comment_factory, apiclient,
+                                                 phase_factory, idea_factory):
+    phase, _, _, idea = setup_phase(
+        phase_factory, idea_factory, CollectPhase
+    )
+    comment = comment_factory(content_object=idea)
+
+    apiclient.force_authenticate(user=comment.creator)
+    url = reverse(
+        'comments-detail',
+        kwargs={
+            'pk': comment.pk,
+            'content_type': comment.content_type.pk,
+            'object_pk': comment.object_pk
+        })
+    data = {'comment': 'comment comment comment'}
+    with freeze_phase(phase):
+        response = apiclient.patch(url, data, format='json')
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['comment'] == 'comment comment comment'
+
+
+@pytest.mark.django_db
+def test_user_can_not_edit_comment_of_other_user(apiclient, user2,
+                                                 comment_factory, idea_factory,
+                                                 phase_factory):
+    phase, _, _, idea = setup_phase(
+        phase_factory, idea_factory, CollectPhase
+    )
+    comment = comment_factory(content_object=idea)
+    apiclient.force_authenticate(user=user2)
+    url = reverse(
+        'comments-detail',
+        kwargs={
+            'pk': comment.pk,
+            'content_type': comment.content_type.pk,
+            'object_pk': comment.object_pk
+        })
+    data = {'comment': 'comment comment comment'}
+
+    with freeze_phase(phase):
+        response = apiclient.patch(url, data, format='json')
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_anonymous_user_can_not_edit_comment(apiclient, comment_factory,
+                                             idea_factory, phase_factory):
+    phase, _, _, idea = setup_phase(
+        phase_factory, idea_factory, CollectPhase
+    )
+    comment = comment_factory(content_object=idea)
+    apiclient.force_authenticate(user=None)
+    url = reverse(
+        'comments-detail',
+        kwargs={
+            'pk': comment.pk,
+            'content_type': comment.content_type.pk,
+            'object_pk': comment.object_pk
+        })
+    data = {'comment': 'comment comment comment'}
+
+    with freeze_phase(phase):
+        response = apiclient.patch(url, data, format='json')
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_admin_of_comment_can_edit_comment(admin, apiclient,
+                                           comment_factory, idea):
+    comment = comment_factory(content_object=idea)
+    url = reverse(
+        'comments-detail',
+        kwargs={
+            'pk': comment.pk,
+            'content_type': comment.content_type.pk,
+            'object_pk': comment.object_pk
+        })
+    data = {'comment': 'comment comment comment'}
+    apiclient.force_authenticate(user=admin)
+    response = apiclient.patch(url, data)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data['comment'] == 'comment comment comment'
+
+
+@pytest.mark.django_db
+def test_moderator_cannot_edit_comment(apiclient, comment_factory,
+                                       idea_factory, phase_factory):
+
+    phase, _, project, idea = setup_phase(
+        phase_factory, idea_factory, CollectPhase
+    )
+
+    _, moderator, _ = setup_users(project)
+
+    comment = comment_factory(content_object=idea)
+
+    apiclient.force_authenticate(user=moderator)
+    url = reverse(
+        'comments-detail',
+        kwargs={
+            'pk': comment.pk,
+            'content_type': comment.content_type.pk,
+            'object_pk': comment.object_pk
+        })
+    data = {'comment': 'comment comment comment'}
+
+    with freeze_phase(phase):
+        response = apiclient.patch(url, data, format='json')
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_initiator_cannot_edit_comment(apiclient, comment_factory,
+                                       idea_factory, phase_factory):
+
+    phase, _, project, idea = setup_phase(
+        phase_factory, idea_factory, CollectPhase
+    )
+
+    _, _, initiator = setup_users(project)
+
+    comment = comment_factory(content_object=idea)
+
+    apiclient.force_authenticate(user=initiator)
+    url = reverse(
+        'comments-detail',
+        kwargs={
+            'pk': comment.pk,
+            'content_type': comment.content_type.pk,
+            'object_pk': comment.object_pk
+        })
+    data = {'comment': 'comment comment comment'}
+
+    with freeze_phase(phase):
+        response = apiclient.patch(url, data, format='json')
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_creater_of_comment_can_set_removed_flag(apiclient, comment_factory,
+                                                 idea_factory, phase_factory):
+    phase, _, project, idea = setup_phase(
+        phase_factory, idea_factory, CollectPhase
+    )
+
+    comment = comment_factory(content_object=idea)
+    url = reverse(
+        'comments-detail',
+        kwargs={
+            'pk': comment.pk,
+            'content_type': comment.content_type.pk,
+            'object_pk': comment.object_pk
+        })
+    apiclient.force_authenticate(user=comment.creator)
+
+    with freeze_phase(phase):
+        response = apiclient.delete(url)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data['is_deleted'] is True
+    assert response.data['comment'] == ''
+
+
+@pytest.mark.django_db
+def test_moderator_of_comment_can_set_censored_flag(apiclient,
+                                                    comment_factory,
+                                                    idea_factory,
+                                                    phase_factory):
+
+    phase, _, project, idea = setup_phase(
+        phase_factory, idea_factory, RatingPhase
+    )
+    _, moderator, _ = setup_users(project)
+    comment = comment_factory(content_object=idea)
+
+    url = reverse(
+        'comments-detail',
+        kwargs={
+            'pk': comment.pk,
+            'content_type': comment.content_type.pk,
+            'object_pk': comment.object_pk
+        })
+    apiclient.force_authenticate(user=moderator)
+    response = apiclient.delete(url)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data['is_deleted'] is True
+    assert response.data['comment'] == ''
