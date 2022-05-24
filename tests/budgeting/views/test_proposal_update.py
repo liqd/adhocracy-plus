fafc598534af2d33202a8f58c2d3f88b01b8afbd
fuzzylogic2000 <k.lindenlaub@liqd.net>
2022-05-24 11:17:52 +0200

    tests/orga terms of use: add tests for all items

--- tests/budgeting/views/test_proposal_update.py
+++ tests/budgeting/views/test_proposal_update.py
@@ -2,70 +2,71 @@ import pytest
 from django.urls import reverse
 
 from adhocracy4.test.helpers import freeze_phase
+from adhocracy4.test.helpers import freeze_post_phase
 from adhocracy4.test.helpers import redirect_target
 from adhocracy4.test.helpers import setup_phase
-from apps.mapideas import models
-from apps.mapideas import phases
+from apps.budgeting import models
+from apps.budgeting import phases
 
 
 @pytest.mark.django_db
 def test_creator_can_update_during_active_phase(client,
                                                 phase_factory,
-                                                map_idea_factory,
+                                                proposal_factory,
                                                 category_factory,
                                                 area_settings_factory):
-    phase, module, project, mapidea = setup_phase(
-        phase_factory, map_idea_factory, phases.IssuePhase)
+    phase, module, project, proposal = setup_phase(
+        phase_factory, proposal_factory, phases.RequestPhase)
     area_settings_factory(module=module)
     category = category_factory(module=module)
-    user = mapidea.creator
+    user = proposal.creator
     url = reverse(
-        'a4_candy_mapideas:mapidea-update',
+        'a4_candy_budgeting:proposal-update',
         kwargs={
-            'organisation_slug': mapidea.project.organisation.slug,
-            'pk': mapidea.pk,
-            'year': mapidea.created.year
+            'organisation_slug': proposal.project.organisation.slug,
+            'pk': proposal.pk,
+            'year': proposal.created.year
         })
     with freeze_phase(phase):
         client.login(username=user.email, password='password')
         data = {
-            'name': 'Another MapIdea',
+            'name': 'Another Proposal',
             'description': 'changed description',
             'category': category.pk,
+            'budget': 123,
             'point': (0, 0),
             'point_label': 'somewhere',
             'organisation_terms_of_use': True,
         }
         response = client.post(url, data)
-        assert redirect_target(response) == 'mapidea-detail'
+        assert redirect_target(response) == 'proposal-detail'
         assert response.status_code == 302
-        updated_mapidea = models.MapIdea.objects.get(id=mapidea.pk)
-        assert updated_mapidea.description == 'changed description'
+        updated_proposal = models.Proposal.objects.get(id=proposal.pk)
+        assert updated_proposal.description == 'changed description'
 
 
 @pytest.mark.django_db
-def test_creator_cannot_update_in_wrong_phase(client,
-                                              phase_factory,
-                                              map_idea_factory,
-                                              category_factory):
-    phase, module, project, mapidea = setup_phase(
-        phase_factory, map_idea_factory, phases.RatingPhase)
+def test_creator_cannot_update_past_phase(
+        client, phase_factory, proposal_factory, category_factory):
+    phase, module, project, proposal = setup_phase(
+        phase_factory, proposal_factory, phases.RequestPhase)
     category = category_factory(module=module)
-    user = mapidea.creator
+    user = proposal.creator
     assert user not in project.moderators.all()
     url = reverse(
-        'a4_candy_mapideas:mapidea-update',
+        'a4_candy_budgeting:proposal-update',
         kwargs={
-            'organisation_slug': mapidea.project.organisation.slug,
-            'pk': mapidea.pk,
-            'year': mapidea.created.year
+            'organisation_slug': proposal.project.organisation.slug,
+            'pk': proposal.pk,
+            'year': proposal.created.year
         })
-    with freeze_phase(phase):
+    with freeze_post_phase(phase):
         client.login(username=user.email, password='password')
         data = {
-            'name': 'Another MapIdea',
+            'name': 'Another Proposal',
             'description': 'changed description',
             'category': category.pk,
+            'budget': 123,
             'organisation_terms_of_use': True,
         }
         response = client.post(url, data)
@@ -73,58 +74,58 @@ def test_creator_cannot_update_in_wrong_phase(client,
 
 
 @pytest.mark.django_db
-def test_moderator_can_update_during_wrong_phase(client,
-                                                 phase_factory,
-                                                 map_idea_factory,
-                                                 category_factory,
-                                                 area_settings_factory):
-    phase, module, project, mapidea = setup_phase(
-        phase_factory, map_idea_factory, phases.RatingPhase)
+def test_moderator_can_update_past_phase(
+        client, phase_factory, proposal_factory, category_factory,
+        area_settings_factory):
+    phase, module, project, proposal = setup_phase(
+        phase_factory, proposal_factory, phases.RequestPhase)
     area_settings_factory(module=module)
     category = category_factory(module=module)
-    user = mapidea.creator
+    user = proposal.creator
     moderator = project.moderators.first()
     assert moderator is not user
     url = reverse(
-        'a4_candy_mapideas:mapidea-update',
+        'a4_candy_budgeting:proposal-update',
         kwargs={
-            'organisation_slug': mapidea.project.organisation.slug,
-            'pk': mapidea.pk,
-            'year': mapidea.created.year
+            'organisation_slug': proposal.project.organisation.slug,
+            'pk': proposal.pk,
+            'year': proposal.created.year
         })
-    with freeze_phase(phase):
+    with freeze_post_phase(phase):
         client.login(username=moderator.email, password='password')
         data = {
-            'name': 'Another MapIdea',
+            'name': 'Another Proposal',
             'description': 'changed description',
             'category': category.pk,
+            'budget': 123,
             'point': (0, 0),
             'point_label': 'somewhere else',
             'organisation_terms_of_use': True,
         }
         response = client.post(url, data)
-        assert redirect_target(response) == 'mapidea-detail'
+        assert redirect_target(response) == 'proposal-detail'
         assert response.status_code == 302
-        updated_mapidea = models.MapIdea.objects.get(id=mapidea.pk)
-        assert updated_mapidea.description == 'changed description'
+        updated_proposal = models.Proposal.objects.get(id=proposal.pk)
+        assert updated_proposal.description == 'changed description'
 
 
 @pytest.mark.django_db
-def test_creator_cannot_update(client, map_idea_factory):
-    mapidea = map_idea_factory()
-    user = mapidea.creator
-    assert user not in mapidea.module.project.moderators.all()
+def test_creator_cannot_update(client, proposal_factory):
+    proposal = proposal_factory()
+    user = proposal.creator
+    assert user not in proposal.module.project.moderators.all()
     url = reverse(
-        'a4_candy_mapideas:mapidea-update',
+        'a4_candy_budgeting:proposal-update',
         kwargs={
-            'organisation_slug': mapidea.project.organisation.slug,
-            'pk': mapidea.pk,
-            'year': mapidea.created.year
+            'organisation_slug': proposal.project.organisation.slug,
+            'pk': proposal.pk,
+            'year': proposal.created.year
         })
     client.login(username=user.email, password='password')
     data = {
-        'name': 'Another MapIdea',
+        'name': 'Another Proposal',
         'description': 'changed description',
+        'budget': 123,
         'organisation_terms_of_use': True,
     }
     response = client.post(url, data)
@@ -133,58 +134,60 @@ def test_creator_cannot_update(client, map_idea_factory):
 
 @pytest.mark.django_db
 def test_moderators_can_always_update(client, phase_factory,
-                                      map_idea_factory,
+                                      proposal_factory,
                                       category_factory,
                                       area_settings_factory):
-    phase, module, project, mapidea = setup_phase(
-        phase_factory, map_idea_factory, phases.RatingPhase)
+    phase, module, project, proposal = setup_phase(
+        phase_factory, proposal_factory, phases.RequestPhase)
     area_settings_factory(module=module)
     category = category_factory(module=module)
-    user = mapidea.creator
+    user = proposal.creator
     moderator = project.moderators.first()
     assert moderator is not user
     url = reverse(
-        'a4_candy_mapideas:mapidea-update',
+        'a4_candy_budgeting:proposal-update',
         kwargs={
-            'organisation_slug': mapidea.project.organisation.slug,
-            'pk': mapidea.pk,
-            'year': mapidea.created.year
+            'organisation_slug': proposal.project.organisation.slug,
+            'pk': proposal.pk,
+            'year': proposal.created.year
         })
     client.login(username=moderator.email, password='password')
     data = {
-        'name': 'Another MapIdea',
+        'name': 'Another Proposal',
         'description': 'changed description',
         'category': category.pk,
+        'budget': 123,
         'point': (0, 0),
         'point_label': 'somewhere',
         'organisation_terms_of_use': True,
     }
     response = client.post(url, data)
-    assert redirect_target(response) == 'mapidea-detail'
+    assert redirect_target(response) == 'proposal-detail'
     assert response.status_code == 302
-    updated_mapidea = models.MapIdea.objects.get(id=mapidea.pk)
-    assert updated_mapidea.description == 'changed description'
+    updated_proposal = models.Proposal.objects.get(id=proposal.pk)
+    assert updated_proposal.description == 'changed description'
 
 
 @pytest.mark.django_db
 def test_moderators_can_update_only_with_terms_agreement(
-        client, map_idea_factory, organisation_terms_of_use_factory,
+        client, proposal_factory, organisation_terms_of_use_factory,
         area_settings_factory):
-    mapidea = map_idea_factory()
-    area_settings_factory(module=mapidea.module)
-    moderator = mapidea.module.project.moderators.first()
-    assert moderator is not mapidea.creator
+    proposal = proposal_factory()
+    area_settings_factory(module=proposal.module)
+    moderator = proposal.module.project.moderators.first()
+    assert moderator is not proposal.creator
     url = reverse(
-        'a4_candy_mapideas:mapidea-update',
+        'a4_candy_budgeting:proposal-update',
         kwargs={
-            'organisation_slug': mapidea.project.organisation.slug,
-            'pk': mapidea.pk,
-            'year': mapidea.created.year
+            'organisation_slug': proposal.project.organisation.slug,
+            'pk': proposal.pk,
+            'year': proposal.created.year
         })
     client.login(username=moderator.email, password='password')
     data = {
         'name': 'Another mappy Idea',
         'description': 'changed description',
+        'budget': 123,
         'point': (0, 0),
     }
     response = client.post(url, data)
@@ -192,11 +195,11 @@ def test_moderators_can_update_only_with_terms_agreement(
 
     organisation_terms_of_use_factory(
         user=moderator,
-        organisation=mapidea.module.project.organisation,
+        organisation=proposal.module.project.organisation,
         has_agreed=True,
     )
     response = client.post(url, data)
-    assert redirect_target(response) == 'mapidea-detail'
+    assert redirect_target(response) == 'proposal-detail'
     assert response.status_code == 302
-    updated_idea = models.MapIdea.objects.get(id=mapidea.pk)
+    updated_idea = models.Proposal.objects.get(id=proposal.pk)
     assert updated_idea.description == 'changed description'
