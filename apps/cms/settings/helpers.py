7dd09c1b83ddf1aff642fc8c5a4bf16544853332
phillimorland <5871230+phillimorland@users.noreply.github.com>
2022-06-02 12:36:02 +0200

    user/forms: add email link to helptext of captcha with contact setting and helper

--- apps/cms/settings/helpers.py
+++ apps/cms/settings/helpers.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.utils.html import mark_safe
 from django.utils.translation import gettext_lazy as _
 from wagtail.core.models import Site
@@ -25,11 +26,21 @@ def add_link_to_helptext(help_text, important_page_name, link_text=None):
     return help_text
 
 
+def add_email_link_to_helptext(help_text, link_text=None):
+    url = settings.CONTACT_EMAIL
+
+    if not link_text:
+        link_text = LINK_TEXT
+    link_text = link_text \
+        .format('<a href="mailto:' + url + '">', '</a>')
+    return mark_safe('{} {}'.format(help_text, link_text))
+
+    return help_text
+
+
 def get_important_page_url(important_page_name):
     site = Site.objects.filter(is_default_site=True).first()
     important_pages = ImportantPages.for_site(site)
     if getattr(important_pages, important_page_name):
         return getattr(important_pages, important_page_name).url
     return None
-
-
