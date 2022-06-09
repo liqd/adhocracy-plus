0ff10dea72ea70c7ad425995a10b0b7c2319ad32
fuzzylogic2000 <k.lindenlaub@liqd.net>
2022-06-09 17:14:05 +0200

    apps/projects/django-admin: make slug editable - fixes #1726

--- apps/projects/admin.py
+++ apps/projects/admin.py
@@ -37,7 +37,7 @@ class ProjectAdmin(admin.ModelAdmin):
 
     fieldsets = (
         (None, {
-            'fields': ('name', 'organisation')
+            'fields': ('name', 'slug', 'organisation')
         }),
         (_('Information and result'), {
             'fields': ('description', 'information', 'result'),
