9df36bf36cad37bd9a4d649611e7e08d3db1971f
Julian Dehm <j.dehm@liqd.net>
2022-06-20 11:23:11 +0200

    unload_warning: rewrite in vanilla js

--- adhocracy-plus/assets/js/unload_warning.js
+++ adhocracy-plus/assets/js/unload_warning.js
@@ -3,16 +3,18 @@
 
 /* global django */
 
-$(function () {
+document.addEventListener('DOMContentLoaded', () => {
   let submitted = false
   const changeHandler = function () {
     const target = event.target.id
     if (target.includes('dashboardToggle')) {
       submitted = true
     }
-    $(window).on('beforeunload', function (e) {
+    window.addEventListener('beforeunload', (e) => {
       if (!submitted) {
+        // string is ignored on most modern browsers
         const string = django.gettext('If you leave this page changes you made will not be saved.')
+        e.preventDefault()
         e.returnValue = string
         return string
       }
@@ -26,11 +28,11 @@ $(function () {
   //   })
   // }
 
-  $(document).one('change', changeHandler)
-    .on('submit', function (e) {
-      if ($(e.target).data('ignore-submit') === true) {
-        return true
-      }
-      submitted = true
-    })
+  document.addEventListener('change', changeHandler, { once: true })
+  document.addEventListener('submit', (e) => {
+    if (e.target.getAttribute('ignore-submit') === true) {
+      return true
+    }
+    submitted = true
+  })
 })
