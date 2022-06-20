5745d5c59204d19dbbeb090c71fde5d50a5f8651
Julian Dehm <j.dehm@liqd.net>
2022-06-20 11:22:33 +0200

    webpack: don't export entry points as library

--- webpack.common.js
+++ webpack.common.js
@@ -112,12 +112,6 @@ module.exports = {
     }
   },
   output: {
-    // exposes exports of entry points
-    library: {
-      name: '[name]',
-      // return value of entry point will be assigned this.
-      type: 'this'
-    },
     // creates a folder to store all assets
     path: path.resolve('./adhocracy-plus/static/'),
     // location they can be accessed, can also be a url
