35ef529fa4a03240af29702482e7b6765f772d5f
phillimorland <5871230+phillimorland@users.noreply.github.com>
2022-07-11 15:23:01 +0200

    broken test

--- apps/documents/assets/__tests__/react_documents_render.jest.jsx
+++ apps/documents/assets/__tests__/react_documents_render.jest.jsx
@@ -3,11 +3,17 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
-// component to be tested
 import ChapterNavItem from '../ChapterNavItem.jsx'
+import ParagraphForm from '../ParagraphForm.jsx'
 
-test('Chapter Nav is showing', () => {
+test('Chapter Nav Item is showing', () => {
   render(<ChapterNavItem />)
   const nav = screen.getByTestId('chapter-nav-item')
+  expect(nav).toBeInTheDocument()
+})
+
+test('Paragraph Form is showing', () => {
+  render(<ParagraphForm />)
+  const nav = screen.getByTestId('paragraph-nav')
   expect(nav).toBeInTheDocument()
 })
