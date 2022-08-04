8e61980a74cbf0f4dd4bbe641f7eb23740e81931
rine <k.schmid@liqd.net>
2022-08-04 18:30:13 +0200

    account//users/serializers: add user_image and user_image_fallback

--- apps/account/serializers.py
+++ apps/account/serializers.py
@@ -5,6 +5,22 @@ from apps.users.models import User
 
 class AccountSerializer(serializers.ModelSerializer):
 
+    user_image = serializers.ImageField(source='_avatar', required=False)
+    user_image_fallback = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ('pk', 'username', '_avatar')
+        fields = ('pk', 'username', 'user_image', 'user_image_fallback')
+
+    def get_user_image_fallback(self, user):
+        """Serve fallback as png as used in app."""
+        try:
+            if user.avatar_fallback_png:
+                url = user.avatar_fallback_png
+                request = self.context.get('request', None)
+                if request is not None:
+                    return request.build_absolute_uri(url)
+                return url
+        except AttributeError:
+            pass
+        return None
--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -5,10 +5,12 @@ from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
     is_self = serializers.SerializerMethodField()
+    user_image = serializers.ImageField(source='_avatar')
+    user_image_fallback = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ('username', '_avatar', 'is_self')
+        fields = ('username', 'user_image', 'user_image_fallback', 'is_self')
 
     def get_is_self(self, instance):
         request = self.context.get('request')
@@ -16,3 +18,16 @@ class UserSerializer(serializers.ModelSerializer):
             user = request.user
             return user == instance
         return False
+
+    def get_user_image_fallback(self, user):
+        """Serve fallback as png as used in app."""
+        try:
+            if user.avatar_fallback_png:
+                url = user.avatar_fallback_png
+                request = self.context.get('request', None)
+                if request is not None:
+                    return request.build_absolute_uri(url)
+                return url
+        except AttributeError:
+            pass
+        return None
