30a33d0da547c0e473841b5af57935209c6fb7ed
hklarner <h.klarner@liqd.net>
2023-07-20 13:44:47 +0200

    apps/projects: adds tests

--- tests/projects/conftest.py
+++ tests/projects/conftest.py
@@ -1,6 +1,21 @@
 from pytest_factoryboy import register
 
+from adhocracy4.test.factories import polls as polls_factory
+from tests.ideas.factories import IdeaFactory
+from tests.interactiveevents import factories as event_factories
+from tests.topicprio.factories import TopicFactory
+
 from . import factories as invites
+
+register(IdeaFactory)
+register(polls_factory.PollFactory)
+register(polls_factory.QuestionFactory)
+register(polls_factory.AnswerFactory)
+register(polls_factory.ChoiceFactory)
+register(polls_factory.VoteFactory)
+register(TopicFactory)
+register(event_factories.LiveQuestionFactory)
+register(event_factories.LikeFactory)
 
 register(invites.ModeratorInviteFactory)
 register(invites.ParticipantInviteFactory)
