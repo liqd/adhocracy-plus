ee16ff4956f694b5d4fe68db5ea29c73f01e280b
hklarner <h.klarner@liqd.net>
2023-07-20 14:11:28 +0200

    apps/projects: removes duplicate code

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -3,7 +3,6 @@ import itertools
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.sites.shortcuts import get_current_site
-from django.core.exceptions import ObjectDoesNotExist
 from django.db import transaction
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
@@ -24,7 +23,6 @@ from adhocracy4.projects.mixins import DisplayProjectOrModuleMixin
 from adhocracy4.projects.mixins import PhaseDispatchMixin
 from adhocracy4.projects.mixins import ProjectMixin
 from adhocracy4.projects.mixins import ProjectModuleDispatchMixin
-from apps import logger
 from apps.projects.models import ProjectInsight
 
 from . import dashboard
@@ -275,6 +273,25 @@ class ProjectDeleteView(PermissionRequiredMixin, generic.DeleteView):
         return super().delete(request, *args, **kwargs)
 
 
+def create_insight_context(insight: ProjectInsight) -> dict:
+    return dict(
+        insight=_(str(insight)),
+        insight_label=_(
+            """This session will provide you with valuable insights
+            into the number of individuals invloved in the process
+            and help you make informed decisions based on the data"""
+        ),
+        counts=[
+            (_("active participants"), insight.active_participants.count()),
+            (_("comments"), insight.comments),
+            (_("ratings"), insight.ratings),
+            (_("written ideas"), insight.written_ideas),
+            (_("poll answers"), insight.poll_answers),
+            (_("interactive event questions"), insight.live_questions),
+        ],
+    )
+
+
 class ProjectDetailView(
     PermissionRequiredMixin, ProjectModuleDispatchMixin, DisplayProjectOrModuleMixin
 ):
@@ -297,30 +314,10 @@ class ProjectDetailView(
         """Append insights to the template context."""
         context = super().get_context_data(**kwargs)
 
-        try:
-            insight = ProjectInsight.objects.get(project=self.project)
-        except ObjectDoesNotExist:
-            logger.warning("WARNING: Project %s does not have insights", self.project)
-        else:
-            if insight.display:
-                insight_title = _(str(insight))
-                insight_label = _(
-                    """This session will provide you with valuable insights
-                    into the number of individuals invloved in the process
-                    and help you make informed decisions based on the data"""
-                )
-                counts = [
-                    (_("active participants"), insight.active_participants.count()),
-                    (_("comments"), insight.comments),
-                    (_("ratings"), insight.ratings),
-                    (_("written ideas"), insight.written_ideas),
-                    (_("poll answers"), insight.poll_answers),
-                    (_("interactive event questions"), insight.live_questions),
-                ]
+        insight, created = ProjectInsight.objects.get_or_create(project=self.project)
+        if insight.display:
+            context.update(create_insight_context(insight=insight))
 
-                context["insight"] = insight_title
-                context["insight_label"] = insight_label
-                context["counts"] = counts
         context["result_title"] = _("Final Results")
 
         return context
@@ -329,35 +326,16 @@ class ProjectDetailView(
 class ProjectResultInsightComponentFormView(ProjectComponentFormView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        try:
-            insight = ProjectInsight.objects.get(project=self.project)
-        except ObjectDoesNotExist:
-            logger.warning("WARNING: Project %s does not have insights", self.project)
-        else:
-            insight_title = _(str(insight))
-            insight_label = _(
-                """This session will provide you with valuable insights
-                into the number of individuals invloved in the process
-                and help you make informed decisions based on the data"""
-            )
-            context["insight"] = insight_title
-            context["insight_label"] = insight_label
-            counts = [
-                (_("active participants"), insight.active_participants.count()),
-                (_("comments"), insight.comments),
-                (_("ratings"), insight.ratings),
-                (_("written ideas"), insight.written_ideas),
-                (_("poll answers"), insight.poll_answers),
-                (_("interactive event questions"), insight.live_questions),
-            ]
+        insight, created = ProjectInsight.objects.get_or_create(project=self.project)
+        context.update(create_insight_context(insight=insight))
 
-            context["counts"] = counts
-            if self.request.POST:
-                context["insight_form"] = dashboard.ProjectInsightForm(
-                    data=self.request.POST, instance=insight
-                )
-            else:
-                context["insight_form"] = dashboard.ProjectInsightForm(instance=insight)
+        if self.request.POST:
+            context["insight_form"] = dashboard.ProjectInsightForm(
+                data=self.request.POST, instance=insight
+            )
+        else:
+            context["insight_form"] = dashboard.ProjectInsightForm(instance=insight)
+
         return context
 
     def form_valid(self, form):
