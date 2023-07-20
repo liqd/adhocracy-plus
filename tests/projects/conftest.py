1b6c3b73fba968c856322c191d4ef345b989ee55
Julian Dehm <j.dehm@liqd.net>
2023-07-20 15:11:14 +0200

    tests: add test for multi-module timeline module detail back button

--- tests/projects/conftest.py
+++ tests/projects/conftest.py
@@ -1,6 +1,9 @@
 from pytest_factoryboy import register
 
+from adhocracy4.test.factories import polls as poll_factories
+
 from . import factories as invites
 
 register(invites.ModeratorInviteFactory)
 register(invites.ParticipantInviteFactory)
+register(poll_factories.PollFactory)
