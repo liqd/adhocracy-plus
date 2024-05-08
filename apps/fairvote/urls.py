5c5da207dbe2d6ea9688ea52544c3b3def3a131a
Oriya <oriya2011@gmail.com>
2024-05-08 13:40:34 +0300

    fair acceptance idea

--- apps/fairvote/urls.py
+++ apps/fairvote/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 
 from .views import ChoinEventListView
 from .views import accepted_ideas
+from .views import ideas_fair_acceptance_order
 
 urlpatterns = [
     path("choinevents/", ChoinEventListView.as_view(), name="choinevent-list"),
     path("accepted_idea_list/<int:obj_id>/", accepted_ideas, name="accepted_ideas"),
+    path(
+        "fair_acceptance_order/<int:obj_id>/",
+        ideas_fair_acceptance_order,
+        name="fair_acceptance_order",
+    ),
 ]
