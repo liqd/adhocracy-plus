43bb17be1f76e1808b3e97a9befb23725966b7ae
Mara Karagianni <m.karagianni@liqd.net>
2024-09-04 17:00:30 +0100

    organisations: add geolocation checkbox in admin

--- adhocracy-plus/config/settings/test.py
+++ adhocracy-plus/config/settings/test.py
@@ -10,3 +10,8 @@ SITE_ID = 1
 CAPTCHA_TEST_ACCEPTED_ANSWER = "testpass"
 CAPTCHA_URL = "https://captcheck.netsyms.com/api.php"
 WAGTAILADMIN_BASE_URL = "http://localhost:8004"
+
+try:
+    from .polygons import *
+except ImportError:
+    pass
