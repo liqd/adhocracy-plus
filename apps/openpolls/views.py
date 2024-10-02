ef781aba8771a64eb3806d28f7a5b9df72b4a8ff
Julian Dehm <j.dehm@liqd.net>
2024-10-02 14:58:58 +0200

    first shot at open poll module

---
+++ apps/openpolls/views.py
@@ -0,0 +1,25 @@
+from django.urls import reverse
+
+from adhocracy4.exports.views import DashboardExportView
+
+
+class PollDashboardExportView(DashboardExportView):
+    template_name = "a4exports/export_dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["comment_export"] = reverse(
+            "a4dashboard:poll-comment-export",
+            kwargs={
+                "organisation_slug": self.module.project.organisation.slug,
+                "module_slug": self.module.slug,
+            },
+        )
+        context["poll_export"] = reverse(
+            "a4dashboard:poll-export",
+            kwargs={
+                "organisation_slug": self.module.project.organisation.slug,
+                "module_slug": self.module.slug,
+            },
+        )
+        return context
