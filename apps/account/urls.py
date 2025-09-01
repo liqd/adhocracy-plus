f352ba2d0cdf63fd64c0e3e4559187ba0a69a0cd
Shane <s.obrien@liqd.net>
2025-09-01 10:32:45 +0200

    apps/notifications - create notification system

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from apps.notifications.views import NotificationSettingsView, NotificationsDashboardView, MarkAllNotificationsAsReadView, MarkNotificationAsReadView
 
 urlpatterns = [
     path("", views.AccountView.as_view(), name="account"),
@@ -15,4 +16,8 @@ urlpatterns = [
         views.OrganisationTermsOfUseUpdateView.as_view(),
         name="user_agreements",
     ),
+    path("notifications/", NotificationsDashboardView.as_view(), name="account_notifications"),
+    path("notifications-settings/", NotificationSettingsView.as_view(), name="account_notification_settings"),
+    path('notifications/mark-as-read/<int:pk>/', MarkNotificationAsReadView.as_view(), name='mark_notification_as_read'),
+    path('notifications/mark-all-as-read/', MarkAllNotificationsAsReadView.as_view(), name='mark_all_notifications_as_read'),
 ]
