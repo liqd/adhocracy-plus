fb60624f4ad99cf5d56a607b5bec88c2906e72e3
Shane <s.obrien@liqd.net>
2025-09-08 16:49:26 +0200

    Add notifications for Moderation feedback and highlighting

--- apps/notifications/signals/comment_signals.py
+++ apps/notifications/signals/comment_signals.py
@@ -1,7 +1,8 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from adhocracy4.comments.models import Comment
-from ..strategies import CommentReplyStrategy, ProjectCommentStrategy
+from ..strategies import CommentReplyStrategy, ProjectCommentStrategy, CommentHighlightedStrategy
+from ..models import NotificationType
 from .helpers import _create_notifications
 
 @receiver(post_save, sender=Comment)
@@ -13,9 +14,25 @@ def handle_comment_notifications(sender, instance, created, **kwargs):
     # Handle comment replies
     if instance.parent_comment.exists():
         strategy = CommentReplyStrategy()
-        _create_notifications(instance, strategy, 'comment_reply')
+        _create_notifications(instance, strategy, NotificationType.COMMENT_REPLY)
     
     # Handle project comments
     elif instance.project and instance.content_object != instance.project:
         strategy = ProjectCommentStrategy()
-        _create_notifications(instance, strategy, 'project_comment')
+        _create_notifications(instance, strategy, NotificationType.COMMENT_ON_POST)
+
+
+@receiver(pre_save, sender=Comment)
+def handle_comment_highlighted(sender, instance, **kwargs):
+    """Handle event update/reschedule notifications"""
+    if instance.id is None:
+        return  # Only handle updates, not creations
+    
+    previous = Comment.objects.get(id=instance.id)
+
+    was_previously_marked = previous.is_moderator_marked
+    is_now_marked = instance.is_moderator_marked
+    # Check if important fields changed
+    if not was_previously_marked and is_now_marked:
+        strategy = CommentHighlightedStrategy()
+        _create_notifications(instance, strategy, 'event_update')
