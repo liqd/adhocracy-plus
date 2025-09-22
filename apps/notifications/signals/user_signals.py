9b44c811fad1dac7fa60e148b94028ecc85b6608
Shane <s.obrien@liqd.net>
2025-09-22 11:20:11 +0200

    Improve styling, 'mark as read' functionality

--- apps/notifications/signals/user_signals.py
+++ apps/notifications/signals/user_signals.py
@@ -1,7 +1,11 @@
-from django.db.models.signals import pre_save, post_save
+from django.db.models.signals import post_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
+
 from apps.users.models import User
+
 from ..models import NotificationSettings
+
 
 @receiver(pre_save, sender=User)
 def create_user_notification_settings(sender, instance, **kwargs):
