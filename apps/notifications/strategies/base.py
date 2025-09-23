692d89187de7d1c9593c73d263520df8cef7693b
Shane <s.obrien@liqd.net>
2025-09-23 11:00:06 +0200

    fix lint

--- apps/notifications/strategies/base.py
+++ apps/notifications/strategies/base.py
@@ -1,21 +1,24 @@
-from abc import ABC, abstractmethod
+from abc import ABC
+from abc import abstractmethod
+
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class BaseNotificationStrategy(ABC):
     """Abstract base class for all notification strategies"""
-    
+
     @abstractmethod
     def get_in_app_recipients(self, obj) -> list[User]:
         """Get users who should receive in-app notifications"""
         pass
-        
+
     @abstractmethod
     def get_email_recipients(self, obj) -> list[User]:
         """Get users who should receive email notifications"""
         pass
-        
+
     @abstractmethod
     def create_notification_data(self, obj) -> dict:
         """Create notification data for a specific recipient"""
