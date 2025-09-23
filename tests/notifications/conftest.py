2c3293557469d013ab1985cd1ecc3a0f53dc2e60
Shane <s.obrien@liqd.net>
2025-09-24 00:14:53 +0200

    add tests

--- tests/notifications/conftest.py
+++ tests/notifications/conftest.py
@@ -3,9 +3,16 @@ from pytest_factoryboy import register
 from tests.budgeting.factories import ProposalFactory
 from tests.ideas.factories import IdeaFactory
 from tests.offlineevents.factories import OfflineEventFactory
+from tests.projects.factories import ModeratorInviteFactory
+from tests.factories import CommentFactory
 from adhocracy4.test import factories as a4_factories
+from tests.factories import ModeratorFeedbackFactory
 
 register(ProposalFactory)
 register(IdeaFactory)
 register(OfflineEventFactory)
 register(a4_factories.PhaseFactory)
+register(CommentFactory)
+register(ModeratorFeedbackFactory)
+register(ModeratorInviteFactory)
+
