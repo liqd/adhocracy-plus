702390ee9fffa0d200451810d3568dea5c7bf645
Shane <s.obrien@liqd.net>
2025-09-24 00:56:24 +0200

    update tests, documentation

--- tests/notifications/conftest.py
+++ tests/notifications/conftest.py
@@ -1,12 +1,14 @@
 from pytest_factoryboy import register
 
+from adhocracy4.test import factories as a4_factories
 from tests.budgeting.factories import ProposalFactory
+from tests.factories import CommentFactory
+from tests.factories import ModeratorFeedbackFactory
 from tests.ideas.factories import IdeaFactory
 from tests.offlineevents.factories import OfflineEventFactory
 from tests.projects.factories import ModeratorInviteFactory
-from tests.factories import CommentFactory
-from adhocracy4.test import factories as a4_factories
-from tests.factories import ModeratorFeedbackFactory
+
+from .factories import NotificationFactory
 
 register(ProposalFactory)
 register(IdeaFactory)
@@ -15,4 +17,4 @@ register(a4_factories.PhaseFactory)
 register(CommentFactory)
 register(ModeratorFeedbackFactory)
 register(ModeratorInviteFactory)
-
+register(NotificationFactory)
