af2c3b8073966a3e5ab24243562b8a1518e024cc
shn-liqd <s.obrien@liqd.net>
2026-01-29 14:03:04 +0100

    [ST-1553] apps/notifications: Bugfixes, improvements

--- tests/notifications/test_moderator_notifications.py
+++ tests/notifications/test_moderator_notifications.py
@@ -11,4 +11,4 @@ def test_notify_moderator_on_create(idea, comment_factory):
 
     moderator_emails = get_emails_for_address(moderator.email)
     assert len(moderator_emails) == 1
-    assert moderator_emails[0].subject.startswith("An Idea was added to the project")
+    assert moderator_emails[0].subject.startswith("An idea was added to the project")
