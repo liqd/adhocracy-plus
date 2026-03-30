01d4fdaaa64c3d3e7587d4819ca190fc69efd3cb
Shane <s.obrien@liqd.net>
2026-03-30 07:47:10 +0200

    apps/landingpage: Include wagtail contact form

--- apps/landingpage/views.py
+++ apps/landingpage/views.py
@@ -2,6 +2,8 @@ import random
 
 from django.shortcuts import render
 
+from apps.cms.contacts.models import FormPage
+
 from .models import StatisticsItem
 
 
@@ -9,9 +11,14 @@ def landing_view(request):
     items = list(StatisticsItem.objects.all().order_by("order"))
     random.shuffle(items)
 
+    form_page = FormPage.objects.get(slug="contact").specific
     # Split into 4 columns
     columns = [items[i::4] for i in range(4)]
     # Alternate directions: down, up, down, up
     directions = ["down", "up", "down", "up"]
 
-    return render(request, "landing.html", {"statistics_columns": zip(columns, directions)})
+    return render(
+        request,
+        "landing.html",
+        {"statistics_columns": zip(columns, directions), "contact_form_page": form_page},
+    )
