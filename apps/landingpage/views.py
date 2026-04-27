bf1e800acdd06fedd8b05932626bcd6c3b82458f
shn-liqd <s.obrien@liqd.net>
2026-04-27 09:40:47 +0200

    apps/landingpage: Fix view to access properties safely (#3070)

--- apps/landingpage/views.py
+++ apps/landingpage/views.py
@@ -3,29 +3,35 @@ import random
 from django.shortcuts import render
 
 from apps.cms.contacts.models import FormPage
-from apps.cms.pages.models import SimplePage
 from apps.cms.news.models import NewsIndexPage
+from apps.cms.pages.models import SimplePage
 
 from .models import StatisticsItem
 
 
 def landing_view(request):
+    # Get statistics
     items = list(StatisticsItem.objects.all().order_by("order"))
     random.shuffle(items)
 
-    # contact form
-    form_page = FormPage.objects.get(slug="contact").specific
+    # Contact form (safe)
+    form_page = FormPage.objects.filter(slug="contact").first()
+    if form_page:
+        form_page = form_page.specific
 
-    # news articles
+    # News articles
     news_index = NewsIndexPage.objects.live().first()
-    latest_news = news_index.news[:3] if news_index else []
-    news_block = {"news_page": news_index, "latest_news": latest_news}
+    if news_index:
+        latest_news = news_index.news[:3]
+        news_block = {"news_page": news_index, "latest_news": latest_news}
+    else:
+        news_block = None
 
+    # FAQ page
     faq_page = SimplePage.objects.live().filter(slug="initiatorguide").first()
 
-    # Split statistics into 4 columns
+    # Split statistics
     columns = [items[i::4] for i in range(4)]
-    # Alternate directions: down, up, down, up
     directions = ["down", "up", "down", "up"]
 
     return render(
