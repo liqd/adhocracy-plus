c99450d4484b0535413bbbabe2fdca3ce5d3ba1d
shn-liqd <s.obrien@liqd.net>
2026-05-12 17:35:20 +0200

    apps/landingpage: fix contact form, add german fields for statisticsItems (#3082)

--- apps/landingpage/models.py
+++ apps/landingpage/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import get_language
 
 
 class StatisticsItem(models.Model):
@@ -12,18 +13,48 @@ class StatisticsItem(models.Model):
     item_type = models.CharField(max_length=20, choices=ITEM_TYPES)
 
     # Text fields
-    header = models.CharField(max_length=200, blank=True)
-    subheader = models.CharField(max_length=300, blank=True)
+    header_en = models.CharField(max_length=200, blank=True)
+    header_de = models.CharField(max_length=200, blank=True)
+    subheader_en = models.CharField(max_length=300, blank=True)
+    subheader_de = models.CharField(max_length=300, blank=True)
 
     # Testimonial fields
-    quote = models.TextField(blank=True)
+    quote_en = models.TextField(blank=True)
+    quote_de = models.TextField(blank=True)
     person_name = models.CharField(max_length=100, blank=True)
-    person_description = models.CharField(max_length=200, blank=True)
     person_image = models.ImageField(upload_to="testimonials/", blank=True, null=True)
+    person_description_en = models.CharField(max_length=200, blank=True)
+    person_description_de = models.CharField(max_length=200, blank=True)
 
     # Image fields
     image = models.ImageField(upload_to="statistics/", blank=True, null=True)
-    image_alt_text = models.TextField(blank=True)
+    image_alt_text_en = models.TextField(blank=True)
+    image_alt_text_de = models.TextField(blank=True)
 
     class Meta:
         ordering = ["order"]
+
+    @property
+    def header(self):
+        lang = get_language()[:2]
+        return getattr(self, f"header_{lang}", self.header_en)
+
+    @property
+    def subheader(self):
+        lang = get_language()[:2]
+        return getattr(self, f"subheader_{lang}", self.subheader_en)
+
+    @property
+    def quote(self):
+        lang = get_language()[:2]
+        return getattr(self, f"quote_{lang}", self.quote_en)
+
+    @property
+    def person_description(self):
+        lang = get_language()[:2]
+        return getattr(self, f"person_description_{lang}", self.person_description_en)
+
+    @property
+    def image_alt_text(self):
+        lang = get_language()[:2]
+        return getattr(self, f"image_alt_text_{lang}", self.image_alt_text_en)
