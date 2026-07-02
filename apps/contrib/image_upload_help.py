031aa98890fd645d75f559c90a081d57186b933d
Grischan <glanzel@gmx.de>
2026-07-02 14:26:25 +0200

    Bring Uppy to all Html Forms

---
+++ apps/contrib/image_upload_help.py
@@ -0,0 +1,11 @@
+from django.utils.translation import gettext_lazy as _
+
+IMAGE_UPLOAD_HELP_TEXT = _(
+    "Allowed file formats are png, jpeg, and gif. "
+    "Images are cropped and resized automatically when you upload them."
+)
+
+IMAGE_UPLOAD_LOGO_HELP_TEXT = _(
+    "The image must be square. Allowed file formats are png, jpeg, and gif. "
+    "Images are cropped and resized automatically when you upload them."
+)
