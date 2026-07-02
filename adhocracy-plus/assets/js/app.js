517b5d162b52e4d05981844b5f59500a972d8829
Grischan <glanzel@gmx.de>
2026-07-02 11:53:27 +0200

    Remove Timline Carousell completly

--- adhocracy-plus/assets/js/app.js
+++ adhocracy-plus/assets/js/app.js
@@ -37,20 +37,6 @@ function init () {
   initProjectDetailParticipationView()
   initGuestProjectAlerts()
 
-  $('.timeline-carousel__item').slick({
-    initialSlide: parseInt($('#timeline-carousel').attr('data-initial-slide'), 10) || 0,
-    focusOnSelect: false,
-    centerMode: true,
-    dots: false,
-    arrows: true,
-    centerPadding: 30,
-    mobileFirst: true,
-    infinite: false,
-    variableWidth: true,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  })
-
   $('.project-tile-carousel').slick({
     initialSlide: 0,
     focusOnSelect: false,
