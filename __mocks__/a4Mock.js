4880cae3244fcb6b89ce31d0a286c671500f0ce6
Shane <s.obrien@liqd.net>
2026-07-13 17:34:24 +0200

    tests: update mocks for new poll component and QuestionImage imports

--- __mocks__/a4Mock.js
+++ __mocks__/a4Mock.js
@@ -1,3 +1,39 @@
+const React = require('react')
+
 module.exports = {
-  alert: () => 'mock alert'
+  __esModule: true,
+
+  // Default export used by default imports (QuestionImage, Alert, api, etc.)
+  default: function (props) {
+    if (props && props.imageUrl) {
+      return React.createElement('div', { className: 'mock-question-image', 'data-prop-alt': props.alt || '' },
+        React.createElement('img', { className: 'mock-question-image-img', src: props.imageUrl, alt: props.alt })
+      )
+    }
+    if (props && props.message) {
+      return React.createElement('div', { 'data-testid': 'alert', onClick: props.onClick }, props.message || props.children || null)
+    }
+    return null
+  },
+
+  // Named exports used by existing moderation tests
+  alert: function () { return 'mock alert' },
+  widget: function () { return null },
+
+  // Named exports used by polls
+  PollOpenQuestion: function (props) {
+    return React.createElement('div', { 'data-testid': 'poll-open-question' })
+  },
+  ConfidentialNotice: function () {
+    return React.createElement('div', { 'data-testid': 'confidential-notice' })
+  },
+  TextareaWithCounter: function (props) {
+    return React.createElement('textarea', { 'data-testid': 'textarea-with-counter', value: props.value, onChange: props.onChange, disabled: props.disabled })
+  },
+  TermsOfUseCheckbox: function () {
+    return React.createElement('div', { 'data-testid': 'terms-of-use-checkbox' })
+  },
+  Alert: function (props) {
+    return React.createElement('div', { 'data-testid': 'alert', onClick: props.onClick }, props.message || props.children || null)
+  }
 }
