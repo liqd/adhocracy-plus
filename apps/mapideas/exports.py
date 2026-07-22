3a1f6e70ff90f9f4256ddd33bd9e5e1ce42f038d
Shane <s.obrien@liqd.net>
2026-07-22 12:18:36 +0200

    apps/exports: include Image export mixin for ideas and proposals

--- apps/mapideas/exports.py
+++ apps/mapideas/exports.py
@@ -13,6 +13,7 @@ class MapIdeaExportView(
     PermissionRequiredMixin,
     mixins.ItemExportWithReferenceNumberMixin,
     mixins.ItemExportWithLinkMixin,
+    mixins.ItemExportWithImageMixin,
     mixins.ExportModelFieldsMixin,
     mixins.ItemExportWithCategoriesMixin,
     mixins.ItemExportWithLabelsMixin,
